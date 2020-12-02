074860ce010e1ed5e8b8949838257bb444630b98
EdenEast <edenofest@gmail.com>
2020-12-02 11:30:53 -0500

    feat: Add nightly neovim overlay

---
+++ nix/overlays/neovim/metadata.nix
@@ -0,0 +1,8 @@
+{
+  upattr = "neovim";
+  repo_git = "https://github.com/neovim/neovim";
+  branch = "nightly";
+  rev = "0a95549d66df63c06d775fcc329f7b63cbb46b2f";
+  sha256 = "sha256-hHpsZYdVwE9tW0WVee3Y55lrrYQYsT+hGH+3MJrspCg=";
+  revdate = "2020-09-14 15:03:02Z";
+}
