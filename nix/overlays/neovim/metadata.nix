bcedcfd4864188fef36da95e7b594efcb3921c9f
James Simpson <edenofest@gmail.com>
2020-12-21 11:47:59 -0500

    chore: Update inputs and neovim

--- nix/overlays/neovim/metadata.nix
+++ nix/overlays/neovim/metadata.nix
@@ -2,7 +2,7 @@
   upattr = "neovim";
   repo_git = "https://github.com/neovim/neovim";
   branch = "nightly";
-  rev = "0a95549d66df63c06d775fcc329f7b63cbb46b2f";
-  sha256 = "sha256-hHpsZYdVwE9tW0WVee3Y55lrrYQYsT+hGH+3MJrspCg=";
-  revdate = "2020-09-14 15:03:02Z";
+  rev = "a1ec36ff31ff1fd545dd135faa47dc782b41a687";
+  sha256 = "sha256-/Ko/Uf15YmysTe6uq1WC6f9GIbbEgfUBDCF6Klm04Xs=";
+  revdate = "2020-12-20";
 }
