e0342308bcbcf518504fb74ed26387fb2f1c9d1f
EdenEast <edenofest@gmail.com>
2020-12-28 01:48:29 -0500

    feat(fzf): Zfh integration

--- home/aspects/shell/fzf.nix
+++ home/aspects/shell/fzf.nix
@@ -10,11 +10,20 @@ in {
   config = mkIf cfg.enable {
     home.packages = [ pkgs.fzf ];
 
-    nyx.aspects.shell.bash.initExtra = ''
-      if [[ :$SHELLOPTS: =~ :(vi|emacs): ]]; then
-        . ${pkgs.fzf}/share/fzf/completion.bash
-        . ${pkgs.fzf}/share/fzf/key-bindings.bash
-      fi
-    '';
+    nyx.aspects.shell.bash.initExtra =
+      mkIf config.nyx.aspects.shell.bash.enable ''
+        if [[ :$SHELLOPTS: =~ :(vi|emacs): ]]; then
+          . ${pkgs.fzf}/share/fzf/completion.bash
+          . ${pkgs.fzf}/share/fzf/key-bindings.bash
+        fi
+      '';
+
+    nyx.aspects.shell.zsh.initExtra =
+      mkIf config.nyx.aspects.shell.zsh.enable ''
+        if [[ $options[zle] = on ]]; then
+          . ${pkgs.fzf}/share/fzf/completion.zsh
+          . ${pkgs.fzf}/share/fzf/key-bindings.zsh
+        fi
+      '';
   };
 }
