29faf0c5b8cb6d32900a622171d51a198fcac871
James Simpson <edenofest@gmail.com>
2021-01-02 23:53:23 -0500

    feat(zsh): Ignore case completion

--- home/files/.config/shell/zsh/003-zsh-settings.zsh
+++ home/files/.config/shell/zsh/003-zsh-settings.zsh
@@ -11,3 +11,7 @@ setopt   HIST_IGNORE_SPACE      # Ignore command if starts with a space
 setopt   HIST_EXPIRE_DUPS_FIRST # Expire duplicate entries first
 setopt   SHARE_HISTORY          # Share history between zsh sessions
 unsetopt EXTENDED_HISTORY       # Make sure no timestamps in histfile
+
+# Ignore case completion for zsh
+zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Z}' '+m:{A-Z}={a-z}'
+
