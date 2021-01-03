d13bc624d1faa6268375dd1e8f73a1b6c6b51c9b
James Simpson <edenofest@gmail.com>
2021-01-03 00:05:24 -0500

    fix(fzf): Show all untracked files with git binding

--- home/files/.config/fzf/functions.sh
+++ home/files/.config/fzf/functions.sh
@@ -11,7 +11,7 @@ fzf-down() {
 
 _gf() {
   is_in_git_repo || return
-  git -c color.status=always status --short |
+  git -c color.status=always status --short --untracked-files=all |
   fzf-down -m --ansi --nth 2..,.. \
     --preview '(git diff --color=always -- {-1} | sed 1,4d; cat {-1}) | head -500' |
   cut -c4- | sed 's/.* -> //'
