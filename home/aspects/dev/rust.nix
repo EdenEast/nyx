1cb8992f020e5d7a11c97a8149a4ec2de808c04a
James Simpson <edenofest@gmail.com>
2021-01-04 13:30:22 -0500

    feat(rust): Add cargo utility pkgs

--- home/aspects/dev/rust.nix
+++ home/aspects/dev/rust.nix
@@ -1,25 +1,38 @@
 { config, lib, pkgs, ... }:
 
 with lib;
-let cfg = config.nyx.aspects.dev.rust;
+let
+  cfg = config.nyx.aspects.dev.rust;
+  cargoPkgs = with pkgs; [
+    cargo-bloat
+    cargo-deny
+    cargo-edit
+    cargo-expand
+    cargo-license
+    cargo-outdated
+    cargo-udeps
+    cargo-whatfeatures
+    cargo-why
+  ];
 in {
   options.nyx.aspects.dev.rust = {
     enable = mkEnableOption "rust configuration";
   };
 
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [
-      # rust toolchain installer
-      rustup
-      # modular compiler frontend and LSP for rust
-      rust-analyzer
-      # ccache with Cloud Storage
-      sccache
+    home.packages = with pkgs;
+      [
+        # rust toolchain installer
+        rustup
+        # modular compiler frontend and LSP for rust
+        rust-analyzer
+        # ccache with Cloud Storage
+        sccache
 
-      # Needed by a lot of crates
-      openssl
-      pkg-config
-    ];
+        # Needed by a lot of crates
+        openssl
+        pkg-config
+      ] ++ cargoPkgs;
 
     home = {
       sessionVariables = {
