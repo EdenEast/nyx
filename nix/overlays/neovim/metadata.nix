80fe8fd3a71cb4a3c8b05c0fd54608f00c20626e
James Simpson <edenofest@gmail.com>
2021-02-08 23:12:05 -0500

    chore: Update neovim

--- nix/overlays/neovim/metadata.nix
+++
@@ -1,8 +0,0 @@
-{
-  upattr = "neovim";
-  repo_git = "https://github.com/neovim/neovim";
-  branch = "nightly";
-  rev = "702208daa6553ecfc3939c3c6e9fcd94fdd5aeba";
-  sha256 = "06nfbvwe1lCvOnA/RnLSonWASfbowMFKVlscdzWzopc=";
-  revdate = "2020-12-20";
-}
