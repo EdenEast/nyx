4f697a1dc6685e28ef633abb9c2389a468cf9686
EdenEast <edenofest@gmail.com>
2021-02-08 23:12:05 -0500

    chore: Update neovim

--- nix/overlays/neovim/default.nix
+++ nix/overlays/neovim/default.nix
@@ -1,14 +1,17 @@
 self: super:
 
-let metadata = import ./metadata.nix;
+let
+  branch = "nightly";
+  rev = "02a3c417945e7b7fc781906a78acbf88bd44c971";
+  sha256 = "3sSRuFF5Vy131D7WjuzzIAxG5JHb1tBzsCXfsJ9L5po=";
 in {
   neovim-unwrapped = super.neovim-unwrapped.overrideAttrs (old: rec {
-    version = "0.5.0-${metadata.branch}";
+    version = "0.5.0-${branch}";
     src = super.fetchFromGitHub {
       owner = "neovim";
       repo = "neovim";
-      rev = metadata.rev;
-      sha256 = metadata.sha256;
+      rev = rev;
+      sha256 = sha256;
     };
 
     buildInputs = old.buildInputs ++ [ super.pkgs.tree-sitter ];
