e2cc14499ddc48c35bb59ac946464165c736c8bf
James Simpson <edenofest@gmail.com>
2021-03-29 19:25:26 -0400

    feat(nvim): Add gitsignsh

--- home/files/.config/nvim/lua/modules/ui/config.lua
+++ home/files/.config/nvim/lua/modules/ui/config.lua
@@ -32,4 +32,37 @@ function config.indentline()
  }
 end
 
+function config.gitsigns()
+  if not packer_plugins['plenary.nvim'].loaded then
+    vim.cmd [[packadd plenary.nvim]]
+  end
+  require('gitsigns').setup {
+    signs = {
+      add = {hl = 'GitGutterAdd', text = '▋'},
+      change = {hl = 'GitGutterChange',text= '▋'},
+      delete = {hl= 'GitGutterDelete', text = '▋'},
+      topdelete = {hl ='GitGutterDeleteChange',text = '▔'},
+      changedelete = {hl = 'GitGutterChange', text = '▎'},
+    },
+    keymaps = {
+       -- Default keymap options
+       noremap = true,
+       buffer = true,
+
+       ['n ]g'] = { expr = true, "&diff ? ']g' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'"},
+       ['n [g'] = { expr = true, "&diff ? '[g' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'"},
+
+       ['n <leader>ghs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+       ['n <leader>ghu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+       ['n <leader>ghr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+       ['n <leader>ghp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+       ['n <leader>ghb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
+
+       -- Text objects
+       ['o ih'] = ':<C-U>lua require"gitsigns".text_object()<CR>',
+       ['x ih'] = ':<C-U>lua require"gitsigns".text_object()<CR>'
+     },
+  }
+end
+
 return config
