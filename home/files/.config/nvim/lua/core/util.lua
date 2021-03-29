0333d8f44324a1308f40d108a4a31e906fa792c2
EdenEast <edenofest@gmail.com>
2021-03-29 19:20:17 -0400

    feat(nvim): Add keymaps

--- home/files/.config/nvim/lua/core/util.lua
+++ home/files/.config/nvim/lua/core/util.lua
@@ -1,3 +1,5 @@
+local global = require('core.global')
+
 local util = {}
 
 util.join = function(tbl, delm)
@@ -11,36 +13,94 @@ util.join = function(tbl, delm)
   return result
 end
 
+util.exec_line = function()
+  local ft = vim.api.nvim_buf_get_option(0, 'filetype')
+  local line = vim.api.nvim_get_current_line()
+
+  if ft == 'lua' then
+    local cmd = string.format('execute(":lua %s")', line)
+    vim.cmd(cmd)
+  elseif ft == 'vim' then
+    local cmd = 'execute ' .. line
+    vim.cmd(cmd)
+  end
+end
+
+util.exec_file = function()
+  local ft = vim.api.nvim_buf_get_option(0, 'filetype')
+  vim.cmd('silent! write')
+
+  if ft == 'lua' then
+    vim.cmd('luafile %')
+  elseif ft == 'vim' then
+    vim.cmd('source %')
+  end
+end
+
+-- Open url cross platform
+util.open_url = function(url)
+  if global.is_windows then
+    vim.cmd([[:execute 'silent !start ]] .. url .. "'")
+  elseif global.is_wsl then
+    vim.cmd([[:execute 'silent !powershell.exe start ]] .. url .. "'")
+  elseif global.is_mac then
+    vim.cmd([[:execute 'silent !open ]] .. url .. "'")
+  elseif global.is_linux then
+    vim.cmd([[:execute 'silent !xdg-open ]] .. url .. "'")
+  else
+    print('Unknown platform. Cannot open url')
+  end
+end
+
+util.open_url_under_cursor = function()
+  local cword = vim.fn.expand('<cWORD>')
+
+  -- Remove surronding quotes if exist
+  local url = string.gsub(cword, [[.*['"](.*)['"].*$]], "%1")
+
+  -- If string starts with https://
+  if string.match(url, [[^https://.*]]) then
+    return util.open_url(url)
+  end
+
+  -- If string matches `user/repo`
+  if string.match(url, [[.*/.*]]) then
+    return util.open_url('https://github.com/' .. url)
+  end
+end
+
+-- Keymap -----------------------------------------------------------------------------------------
+
 util.map = function(mode, lhs, rhs, opts)
   local options = {noremap = true, silent = true}
-  if opts then options = tim.tbl_extend('force', options, opts) end
+  if opts then options = vim.tbl_extend('force', options, opts) end
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 -- Window maps
 util.wmap = function(mode, lhs, rhs, opts)
-  return map(mode, lhs, '<cmd>wincmd '..rhs..'<CR>', opts)
+  return util.map(mode, lhs, '<cmd>wincmd '..rhs..'<CR>', opts)
 end
 
 util.nmap = function(lhs, rhs, opts)
-  return map('n', lhs, rhs, opts)
+  return util.map('n', lhs, rhs, opts)
 end
 
 util.imap = function(lhs, rhs, opts)
-  return map('i', lhs, rhs, opts)
+  return util.map('i', lhs, rhs, opts)
 end
 
 util.xmap = function(lhs, rhs, opts)
-  return map('x', lhs, rhs, opts)
+  return util.map('x', lhs, rhs, opts)
 end
 
 util.tmap = function(lhs, rhs, opts)
-  return map('t', lhs, rhs, opts)
+  return util.map('t', lhs, rhs, opts)
 end
 
 util.cmap = function(lhs, rhs)
   -- { silent } need to be false to work
-  return map('c', lhs, rhs, { silent = false })
+  return util.map('c', lhs, rhs, { silent = false })
 end
 
 return util
