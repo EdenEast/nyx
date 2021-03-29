9e9183ecc0ce084a761dceb318d9a36f8dd6e2c0
EdenEast <edenofest@gmail.com>
2021-03-29 19:18:36 -0400

    feat(nvim): Add keymap utils

--- home/files/.config/nvim/lua/core/util.lua
+++ home/files/.config/nvim/lua/core/util.lua
@@ -11,4 +11,36 @@ util.join = function(tbl, delm)
   return result
 end
 
+util.map = function(mode, lhs, rhs, opts)
+  local options = {noremap = true, silent = true}
+  if opts then options = tim.tbl_extend('force', options, opts) end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+-- Window maps
+util.wmap = function(mode, lhs, rhs, opts)
+  return map(mode, lhs, '<cmd>wincmd '..rhs..'<CR>', opts)
+end
+
+util.nmap = function(lhs, rhs, opts)
+  return map('n', lhs, rhs, opts)
+end
+
+util.imap = function(lhs, rhs, opts)
+  return map('i', lhs, rhs, opts)
+end
+
+util.xmap = function(lhs, rhs, opts)
+  return map('x', lhs, rhs, opts)
+end
+
+util.tmap = function(lhs, rhs, opts)
+  return map('t', lhs, rhs, opts)
+end
+
+util.cmap = function(lhs, rhs)
+  -- { silent } need to be false to work
+  return map('c', lhs, rhs, { silent = false })
+end
+
 return util
