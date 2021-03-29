4a255b9d56c2e2d89ebb2b50cc0d5cac23068ce5
James Simpson <edenofest@gmail.com>
2021-03-29 19:26:55 -0400

    feat(nvim): Add opt function that will be merged soon

--- home/files/.config/nvim/lua/core/options/init.lua
+++ home/files/.config/nvim/lua/core/options/init.lua
@@ -1,3 +1,22 @@
+-- TODO: Remove when https://github.com/neovim/neovim/pull/13479 lands
+local opts_info = vim.api.nvim_get_all_options_info()
+local opt =
+  setmetatable(
+  {},
+  {
+    __index = vim.o,
+    __newindex = function(_, key, value)
+      vim.o[key] = value
+      local scope = opts_info[key].scope
+      if scope == "win" then
+        vim.wo[key] = value
+      elseif scope == "buf" then
+        vim.bo[key] = value
+      end
+    end
+  }
+)
+
 local bind = function(tbl)
 	for name, value in pairs(tbl) do
 		vim.o[name] = value
