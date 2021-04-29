b255107cd4c7dd412be3e5383368edb85cf69a19
EdenEast <edenofest@gmail.com>
2021-04-28 23:54:58 -0400

    feat: Add signing commit to kiiro

--- hosts/kiiro/default.nix
+++ hosts/kiiro/default.nix
@@ -10,6 +10,9 @@
           git = {
             userName = "James Simpson";
             userEmail = "edenofest@gmail.com";
+            signing = {
+              key = "5A038CEFD458DB47A6135B3F8316DECECB1A3F10";
+            };
           };
 
           gnupg = {
