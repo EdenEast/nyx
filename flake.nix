38be92160a891a22a85a0dc56e401b4398eb54f4
James Simpson <edenofest@gmail.com>
2021-06-06 00:34:50 -0400

    feat: Init flake with inputs

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = ''
+    Nyx is the personal configuration for James Simpson. This repository holdes .dotfile configuration as well as both
+    nix (with home-manager) and nixos configurations.
+  '';
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-compat.flake = false;
+
+    # Overlays
+    neovim-nightly.url = "github:nix-community/neovim-nightly-overlay";
+  };
+
+
+  outputs = { self, ... }@inputs: {
+    packages.x86_64-linux.hello = inputs.nixpkgs.legacyPackages.x86_64-linux.hello;
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
+  };
+}
