488b18d0525b288fc28b2c70026cefec66829b06
EdenEast <edenofest@gmail.com>
2021-06-08 09:51:00 -0400

    feat: Add pre-built tree-sitter parsers

--- home/modules/shell/neovim.nix
+++ home/modules/shell/neovim.nix
@@ -11,6 +11,25 @@ in {
     # home.packages = with pkgs; [ neovim-unwrapped ];
     home.packages = with pkgs; [ neovim-nightly ];
     xdg.configFile."nvim".source = ../../../config/.config/nvim;
+
+    # Add Treesitter parsers
+    home.file = {
+      "${config.xdg.dataHome}/nvim/parser/bash.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-bash}/parser";
+      "${config.xdg.dataHome}/nvim/parser/c.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-c}/parser";
+      "${config.xdg.dataHome}/nvim/parser/cpp.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-cpp}/parser";
+      "${config.xdg.dataHome}/nvim/parser/css.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-css}/parser";
+      "${config.xdg.dataHome}/nvim/parser/go.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-go}/parser";
+      "${config.xdg.dataHome}/nvim/parser/haskell.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-haskell}/parser";
+      "${config.xdg.dataHome}/nvim/parser/javascript.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-javascript}/parser";
+      "${config.xdg.dataHome}/nvim/parser/json.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-json}/parser";
+      "${config.xdg.dataHome}/nvim/parser/lua.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-lua}/parser";
+      "${config.xdg.dataHome}/nvim/parser/markdown.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-markdown}/parser";
+      "${config.xdg.dataHome}/nvim/parser/nix.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-nix}/parser";
+      "${config.xdg.dataHome}/nvim/parser/python.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-python}/parser";
+      "${config.xdg.dataHome}/nvim/parser/regex.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-regex}/parser";
+      "${config.xdg.dataHome}/nvim/parser/rust.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-rust}/parser";
+      "${config.xdg.dataHome}/nvim/parser/typescript.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-typescript}/parser";
+    };
   };
 }
 
