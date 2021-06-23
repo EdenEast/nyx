b0f710ea86874f981552c174aef2ee4bf54bf305
EdenEast <edenofest@gmail.com>
2021-06-23 10:50:17 -0400

    chore: Update flake inputs

--- home/modules/shell/direnv.nix
+++ home/modules/shell/direnv.nix
@@ -10,7 +10,7 @@ in {
   config = mkIf cfg.enable {
     programs.direnv = {
       enable = true;
-      enableNixDirenvIntegration = true;
+      nix-direnv.enable = true;
     };
 
     nyx.modules.shell.bash.initExtra =
