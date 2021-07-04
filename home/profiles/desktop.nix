7c79d0afb764191db99b3623a8b1bbffae58132a
EdenEast <edenofest@gmail.com>
2021-07-03 20:38:19 -0400

    feat: Add synergy to network keyboard and mouse

--- home/profiles/desktop.nix
+++ home/profiles/desktop.nix
@@ -6,6 +6,9 @@ in {
   options.nyx.profiles.desktop = { enable = mkEnableOption "desktop profile"; };
 
   config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      synergy
+    ];
     nyx.modules = {
       app.alacritty.enable = true;
       app.discord.enable = true;
