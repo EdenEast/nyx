964f3aa7d70842208e2171d226e2ce07d91d273d
EdenEast <edenofest@gmail.com>
2021-07-05 11:32:31 -0400

    feat: Expose nyx as a flake

--- lib/mkHostConfig.nix
+++ lib/mkHostConfig.nix
@@ -1,5 +1,6 @@
-{ lib, ... }:
+{ lib, inputs, ... }:
 
+with inputs.nixpkgs.lib;
 {
   # Creates a host configuration that can them be used by either the mkHomeConfiguration, or mkSystemConfiguration
   # functions. These are internal abstractions.
@@ -10,5 +11,32 @@
         (import ../home/profiles)
         (config)
       ];
+
+      # Reexpose self and nixpkgs as a flake
+      xdg.configFile."nix/registry.json".text = builtins.toJSON {
+        version = 2;
+        flakes =
+          let
+            toInput = input:
+              {
+                type = "path";
+                path = input.outPath;
+              } // (
+                filterAttrs
+                  (n: _: n == "lastModified" || n == "rev" || n == "revCount" || n == "narHash")
+                  input
+              );
+          in
+          [
+            {
+              from = { id = "self"; type = "indirect"; };
+              to = toInput inputs.self;
+            }
+            {
+              from = { id = "nixpkgs"; type = "indirect"; };
+              to = toInput inputs.nixpkgs;
+            }
+          ];
+      };
     });
 }
