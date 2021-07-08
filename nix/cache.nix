09a1d80a1cd3995e6bcbdf08617978d5abc522e5
EdenEast <edenofest@gmail.com>
2021-07-08 00:21:43 -0400

    feat: Add fenix as the rust target
    This change also adds binary cachix servers from nix-community

---
+++ nix/cache.nix
@@ -0,0 +1,10 @@
+{
+  binaryCache = [
+    "https://cachix.cachix.org"
+    "https://nix-community.cachix.org"
+  ];
+  binaryCachePublicKeys = [
+    "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+  ];
+}
