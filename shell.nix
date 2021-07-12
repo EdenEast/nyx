04d20903769e1e78f68a8900d99aaf43baa7261a
EdenEast <edenofest@gmail.com>
2021-07-12 12:47:38 -0400

    feat: Add nix-shell and devShell

---
+++ shell.nix
@@ -0,0 +1,23 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  nixConf = import ./nix/conf.nix;
+  options = [
+    ''--option extra-substituters "${builtins.concatStringsSep " " nixConf.binaryCaches}"''
+    ''--option extra-trusted-public-keys "${builtins.concatStringsSep " " nixConf.binaryCachePublicKeys}"''
+    ''--option experimental-features "nix-command flakes"''
+  ];
+in
+pkgs.mkShell {
+  name = "nyx";
+  nativeBuildInputs = with pkgs; [
+    git
+    git-crypt
+    nixUnstable
+  ];
+
+  shellHook = ''
+    PATH=${pkgs.writeShellScriptBin "nix" ''
+    ${pkgs.nixFlakes}/bin/nix ${builtins.concatStringsSep " " options} "$@"
+  ''}/bin:$PATH
+  '';
+}
