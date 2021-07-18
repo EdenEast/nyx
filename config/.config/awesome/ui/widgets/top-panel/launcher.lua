042890e4ad7a4280f233f1ebe76f46df9ae02224
James Simpson <edenofest@gmail.com>
2021-07-18 00:43:17 -0400

    chore: Move bling, and widget folders
    bling has been moved into modules folder.
    widgets have been moved into ui

--- config/.config/awesome/ui/widgets/top-panel/launcher.lua
+++ config/.config/awesome/ui/widgets/top-panel/launcher.lua
@@ -1,5 +1,5 @@
 local awful = require("awful")
 local beautiful = require("beautiful")
-require("widgets.mainmenu")
+require("ui.widgets.mainmenu")
 
 mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon, menu = mymainmenu })
