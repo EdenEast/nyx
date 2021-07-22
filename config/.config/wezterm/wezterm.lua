73ad3ccee55884674ce0be2cf009b3f6120d3c11
James Simpson <edenofest@gmail.com>
2021-07-22 11:31:19 -0400

    feat: Add Launcher key to wezterm
    Currently I have the launcher key bound to ctrl + \ key as I think that
    would be out of the way. This is useful in windows if I need powershell
    for what ever reason.
    
    I have set the default shell to be wsl.

--- config/.config/wezterm/wezterm.lua
+++ config/.config/wezterm/wezterm.lua
@@ -1,6 +1,35 @@
 local wt = require("wezterm")
 
-return {
+local function windows_launch_menu()
+  local launch_menu = { {
+    label = "Powershell",
+    args = { "powershell.exe", "--NoLogo" },
+  } }
+
+  local success, wsl_list, wsl_err = wt.run_child_process({ "wsl.exe", "-l" })
+
+  -- `wsl.exe -l` has a bug where it always outputs utf16:
+  -- https://github.com/microsoft/WSL/issues/4607
+  -- So we get to convert it
+  wsl_list = wt.utf16_to_utf8(wsl_list)
+
+  for idx, line in ipairs(wt.split_by_newlines(wsl_list)) do
+    -- The first line is a heading
+    if idx > 1 then
+      local distro = line:gsub(" %(Default%)", "")
+      if not distro:find("^docker") then
+        table.insert(launch_menu, {
+          label = distro,
+          args = { "wsl.exe", "--distribution", distro },
+        })
+      end
+    end
+  end
+
+  return launch_menu
+end
+
+local config = {
   colors = {
     background = "#192330",
     foreground = "#afc0d5",
@@ -20,9 +49,22 @@ return {
 
   hide_tab_bar_if_only_one_tab = true,
 
+  keys = {
+    { key = "\\", mods = "ALT", action = "ShowLauncher" },
+  },
+
   window_padding = {
     left = 5,
     right = 5,
     bottom = 5,
   },
 }
+
+-- Windows --------------------------------------------------------------------
+
+if wt.target_triple == "x86_64-pc-windows-msvc" then
+  config.launch_menu = windows_launch_menu()
+  config.default_prog = { "wsl.exe" }
+end
+
+return config
