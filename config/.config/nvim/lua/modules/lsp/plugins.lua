3c764ba2a60827a875185424a77dc68ed964f64f
James Simpson <edenofest@gmail.com>
2021-08-18 11:53:32 -0400

    chore(nvim): Remove unneeded `modules/lsp/config`
    This file was just requiring other files and can be done from the
    `plugins.lua` file.

--- config/.config/nvim/lua/modules/lsp/plugins.lua
+++ config/.config/nvim/lua/modules/lsp/plugins.lua
@@ -1,11 +1,10 @@
 local lsp = {}
-local conf = require("modules.lsp.config")
 local pack = require("core.pack")
 
 -- Nvim Lsp ---------------------------------------------------------------------------------------
 
 lsp["neovim/nvim-lspconfig"] = {
-  config = conf.nvim_lsp,
+  config = [[require("modules.lsp.lspconfig")]],
   requires = {
     "nvim-lua/lsp_extensions.nvim",
     "glepnir/lspsaga.nvim",
@@ -16,7 +15,7 @@ lsp["neovim/nvim-lspconfig"] = {
 
 lsp["hrsh7th/nvim-compe"] = {
   event = "InsertEnter",
-  config = conf.nvim_compe,
+  config = [[require("modules.lsp.complete")]],
   requires = {
     { "hrsh7th/vim-vsnip", opt = true },
     { "hrsh7th/vim-vsnip-integ", opt = true },
