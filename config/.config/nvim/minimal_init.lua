82e96723275b8a68a461aeb4c048ec642bb2932a
EdenEast <edenofest@gmail.com>
2021-08-21 16:07:02 -0400

    test(nvim): Add `minimal_init.lua`
    This is an example of a minimal init

---
+++ config/.config/nvim/minimal_init.lua
@@ -0,0 +1,28 @@
+-- Minimal init.lua for checking neovim issues
+vim.cmd([[set runtimepath=$VIMRUNTIME]])
+vim.cmd([[set packpath=/tmp/nvim/site]])
+local package_root = "/tmp/nvim/site/pack"
+local install_path = package_root .. "/packer/start/packer.nvim"
+local function load_plugins()
+  require("packer").startup({
+    {
+      "wbthomason/packer.nvim",
+      -- ADD PLUGINS THAT ARE _NECESSARY_ FOR REPRODUCING THE ISSUE
+    },
+    config = {
+      package_root = package_root,
+      compile_path = install_path .. "/plugin/packer_compiled.lua",
+      display = { non_interactive = true },
+    },
+  })
+end
+_G.load_config = function()
+  -- ADD INIT.LUA SETTINGS THAT ARE _NECESSARY_ FOR REPRODUCING THE ISSUE
+end
+if vim.fn.isdirectory(install_path) == 0 then
+  print("Installing Telescope and dependencies.")
+  vim.fn.system({ "git", "clone", "--depth=1", "https://github.com/wbthomason/packer.nvim", install_path })
+end
+load_plugins()
+require("packer").sync()
+vim.cmd([[autocmd User PackerComplete ++once echo "Ready!" | lua load_config()]])
