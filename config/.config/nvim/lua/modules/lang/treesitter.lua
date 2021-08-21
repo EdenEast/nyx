276415c359858e0fa589b336a0d88fd51c8b013d
James Simpson <edenofest@gmail.com>
2021-08-21 16:49:50 -0400

    fix(nvim): Fix spelling mistake in treesitter

--- config/.config/nvim/lua/modules/lang/treesitter.lua
+++ config/.config/nvim/lua/modules/lang/treesitter.lua
@@ -26,7 +26,7 @@ require("nvim-treesitter.configs").setup({
     select = {
       enable = true,
       lookahead = true,
-      keymap = {
+      keymaps = {
         ["ia"] = "@parameter.inner",
         ["aa"] = "@parameter.outer",
         ["af"] = "@function.outer",
