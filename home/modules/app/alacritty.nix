ef2ca80473b59797425d0e31521585a7a6458d20
James Simpson <edenofest@gmail.com>
2021-08-26 16:17:39 -0400

    feat(alacritty): Add font size option to nix mod
    This lets me set alacritty settings for each system I am on. For now I
    have started with the font size as different machines need different
    font sizes.

--- home/modules/app/alacritty.nix
+++ home/modules/app/alacritty.nix
@@ -1,15 +1,31 @@
 { config, lib, pkgs, ... }:
 
 with lib;
-let cfg = config.nyx.modules.app.alacritty;
+let
+  cfg = config.nyx.modules.app.alacritty;
 in
 {
   options.nyx.modules.app.alacritty = {
     enable = mkEnableOption "alacritty configuration";
+    fontSize = mkOption {
+      description = "Override font size";
+      type = with types; nullOr int;
+      default = null;
+    };
   };
 
   config = mkIf cfg.enable {
     home.packages = [ pkgs.alacritty ];
     xdg.configFile."alacritty".source = ../../../config/.config/alacritty;
+
+    xdg.dataFile."alacritty/nyx-config.yml".text = let
+      fontText = if cfg.fontSize != null then ''
+        font:
+          size: ${toString cfg.fontSize}
+      '' else "";
+    in
+      ''
+        ${fontText}
+      '';
   };
 }
