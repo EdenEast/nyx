1c8420f48d28c10cd92668dd68a8b6e3030bea84
EdenEast <edenofest@gmail.com>
2021-09-06 00:20:16 -0400

    fix(nvim): Debug util, fix foreach loop

--- config/.config/nvim/lua/modules/editor/debug/util.lua
+++ config/.config/nvim/lua/modules/editor/debug/util.lua
@@ -4,18 +4,18 @@ local M = {}
 
 M.user_select = function(prompt, options)
   local choices = { prompt }
-  for i, line in pairs(options) do
+  for i, line in ipairs(options) do
     if string.len(line) > 0 then
       table.insert(choices, fmt("%d %s", i, line))
     end
-
-    local choice = vim.fn.inputlist(choices)
-    if choice < 1 or choice > #options then
-      return nil
-    end
-
-    return options[choice]
   end
+
+  local choice = vim.fn.inputlist(choices)
+  if choice < 1 or choice > #options then
+    return nil
+  end
+
+  return options[choice]
 end
 
 M.basic_file_path = function()
