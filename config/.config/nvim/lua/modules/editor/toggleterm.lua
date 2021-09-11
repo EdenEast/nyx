2889cf289b840e0b252a5ed02e2c1398e8c4279c
EdenEast <edenofest@gmail.com>
2021-09-11 00:24:08 -0400

    feat(nvim): Custom toggle terminal
    This change was made as there is a issue with neovim and setting your
    vim.o.shell to powershell. When powershell is set to your shell calling
    vim.fn.termopen('powershell') will fail. This change checks to see if we
    are on windows, if so then set the command to startup the terminal to be
    powershell. This works around the issue.
    
    This change also introduces the opportunity to create other togglable
    terminals. Maybe a floating scratch pad?

---
+++ config/.config/nvim/lua/modules/editor/toggleterm.lua
@@ -0,0 +1,47 @@
+local global = require("core.global")
+local Terminal = require("toggleterm.terminal").Terminal
+
+require("toggleterm").setup({
+  size = function(term)
+    if term.direction == "horizontal" then
+      return 20
+    elseif term.direction == "vertical" then
+      return vim.o.columns * 0.4
+    end
+  end,
+  direction = "horizontal",
+  float_opts = {
+    border = "double",
+    highlights = {
+      background = "DarkenedPanel",
+      border = "DarkenedPanel",
+    },
+  },
+})
+
+local shell = global.is_windows and "powershell -NoLogo" or vim.o.shell
+
+local terminals = {}
+
+terminals.default = Terminal:new({
+  cmd = shell,
+  direction = "horizontal",
+  -- function to run on opening the terminal
+  on_open = function(term)
+    vim.cmd("startinsert!")
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+    vim.api.nvim_buf_set_keymap(
+      term.bufnr,
+      "t",
+      "<c-space>",
+      [[<C-\><C-n><cmd>lua toggle_terminal('default')<cr>]],
+      { noremap = true, silent = true }
+    )
+  end,
+})
+
+_G.toggle_terminal = function(name)
+  terminals[name]:toggle()
+end
+
+vim.keymap.nnoremap({ "<c-space>", [[<cmd>lua toggle_terminal('default')<cr>]] })
