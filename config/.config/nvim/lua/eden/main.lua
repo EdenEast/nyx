603beaa8217c73d55bfd40f66d86e89291dafe97
EdenEast <edenofest@gmail.com>
2021-09-26 17:46:07 -0400

    feat(nvim): Add start of modules

--- config/.config/nvim/lua/eden/main.lua
+++ config/.config/nvim/lua/eden/main.lua
@@ -1,6 +1,7 @@
 require("eden.core.theme")
-require("eden.user.keymap")
 require("eden.user.event")
+require("eden.user.keymap")
 require("eden.user.options")
+require("eden.core.pack").trigger_after()
 
 require("packer_compiled")
