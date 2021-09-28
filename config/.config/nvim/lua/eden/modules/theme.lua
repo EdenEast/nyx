13034053434aab32c57e9899061b406d5e35261e
EdenEast <edenofest@gmail.com>
2021-09-28 19:36:49 -0400

    feat(nvim): Add shade plugin to dim unfocused bufs

--- config/.config/nvim/lua/eden/modules/theme.lua
+++ config/.config/nvim/lua/eden/modules/theme.lua
@@ -19,6 +19,21 @@ M.plugins = {
   },
 
   { "embark-theme/vim", "eddyekofo94/gruvbox-flat.nvim" },
+
+  {
+    "sunjon/shade.nvim",
+    config = function()
+      require("shade").setup({
+        overlay_opacity = 75,
+        opacity_step = 1,
+        keys = {
+          brightness_up = "<C-Up>",
+          brightness_down = "<C-Down>",
+          toggle = "<Leader>ts",
+        },
+      })
+    end,
+  },
 }
 
 return M
