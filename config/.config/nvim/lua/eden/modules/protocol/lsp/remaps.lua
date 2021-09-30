cbc948e39d9bf53badfdb89a33e42ff42b1e61b9
James Simpson <edenofest@gmail.com>
2021-09-29 23:05:06 -0400

    fix(nvim): Remove lspsaga

--- config/.config/nvim/lua/eden/modules/protocol/lsp/remaps.lua
+++ config/.config/nvim/lua/eden/modules/protocol/lsp/remaps.lua
@@ -4,27 +4,59 @@ M.set = function(client, bufnr)
   edn.keymap({
     buffer = true,
     {
-      { "K", [[<cmd>lua require('lspsaga.hover').render_hover_doc()<cr>]] },
-      { "<c-f>", [[<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<cr>]] },
-      { "<c-b>", [[<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<cr>]] },
-
-      { "<c-s>", [[<cmd>lua vim.lsp.buf.signature_help()<cr>]], mode = "i" },
-      { "gD", [[<cmd>lua vim.lsp.buf.declaration()<cr>]] },
-      { "gi", [[<cmd>lua vim.lsp.buf.implementation()<cr>]] },
-      { "gy", [[<cmd>lua vim.lsp.buf.type_definition()<cr>]] },
-      { "ga", [[<cmd>lua require('lspsaga.codeaction').code_action()<cr>]] },
-
-      { "[e", [[<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>]] },
-      { "]e", [[<cmd>lua vim.lsp.diagnostic.goto_next()<cr>]] },
-
-      { "<leader>cn", [[<cmd>lua require('lspsaga.rename').rename()<cr>]] },
-      -- { "<leader>ca", [[<cmd>lua require('lspsaga.codeaction').code_action()<cr>]] },
-      -- { "<leader>ca", [[:<c-u><cmd>lua require('lspsaga.codeaction').range_code_action()<cr>]], mode = "v" },
-      { "<leader>ca", [[<cmd>CodeActionMenu<cr>]], mode = "nv" },
-
+      {
+        "K",
+        function()
+          vim.lsp.buf.hover()
+        end,
+      },
+      {
+        "<c-s>",
+        function()
+          vim.lsp.buf.signature_help()
+        end,
+        mde = "i",
+      },
       { "gd", [[<cmd>Telescope lsp_definitions<cr>]] },
       { "gr", [[<cmd>Telescope lsp_references<cr>]] },
+      {
+        "gD",
+        function()
+          vim.lsp.buf.declaration()
+        end,
+      },
+      {
+        "gi",
+        function()
+          vim.lsp.buf.implementation()
+        end,
+      },
+      {
+        "gy",
+        function()
+          vim.lsp.buf.type_definition()
+        end,
+      },
+      { "ga", [[<cmd>CodeActionMenu<cr>]], mode = "nv" },
+      {
+        "[e",
+        function()
+          vim.lsp.diagnostic.goto_prev()
+        end,
+      },
+      {
+        "]e",
+        function()
+          vim.lsp.diagnostic.goto_next()
+        end,
+      },
       { "<leader>ce", [[<cmd>Telescope lsp_workspace_diagnostics<cr>]] },
+      {
+        "<leader>cn",
+        function()
+          vim.lsp.buf.rename()
+        end,
+      },
     },
   })
 end
