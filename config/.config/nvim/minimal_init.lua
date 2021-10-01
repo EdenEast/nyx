693bd3668d354c69f2e3604260c723bb1ac23a98
James Simpson <edenofest@gmail.com>
2021-10-01 16:45:07 -0400

    chore(nvim): Cleanup minimal_init

--- config/.config/nvim/minimal_init.lua
+++ config/.config/nvim/minimal_init.lua
@@ -1,26 +1,25 @@
--- Minimal init.lua for checking neovim issues
-vim.cmd([[set runtimepath=$VIMRUNTIME]])
-vim.cmd([[set packpath=/tmp/nvim/site]])
-local package_root = "/tmp/nvim/site/pack"
-local install_path = package_root .. "/packer/start/packer.nvim"
+-- `minimal_init.lua` used for reproducing neovim issues
+-- Open with `nvim --clean -u minimal_init.lua`
+
+local install_path = "/tmp/nvim/site/pack/packer/start/packer.nvim"
+local compile_path = install_path .. "/plugin/packer_compiled.lua"
+vim.opt.packpath = "/tmp/nvim/site"
+
 local function load_plugins()
   require("packer").startup({
-    {
-      "wbthomason/packer.nvim",
+    function(use)
+      use("wbthomason/packer.nvim")
       -- ADD PLUGINS THAT ARE _NECESSARY_ FOR REPRODUCING THE ISSUE
-    },
-    config = {
-      package_root = package_root,
-      compile_path = install_path .. "/plugin/packer_compiled.lua",
-      display = { non_interactive = true },
-    },
+    end,
+    config = { compile_path = compile_path, package_root = "/tmp/nvim/site/pack" },
   })
 end
+
 _G.load_config = function()
   -- ADD INIT.LUA SETTINGS THAT ARE _NECESSARY_ FOR REPRODUCING THE ISSUE
 end
+
 if vim.fn.isdirectory(install_path) == 0 then
-  print("Installing dependencies.")
   vim.fn.system({ "git", "clone", "--depth=1", "https://github.com/wbthomason/packer.nvim", install_path })
 end
 load_plugins()
