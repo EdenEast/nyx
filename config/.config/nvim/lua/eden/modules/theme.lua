a12cf5aef7c3081bd74538dbac585e50a8398f8f
James Simpson <edenofest@gmail.com>
2021-10-01 10:40:58 -0400

    fix(nvim): Remove shade brightness keymappings
    These key mapping were colliding when using colemak keyboard and using
    arrow keys as hjkl.

--- config/.config/nvim/lua/eden/modules/theme.lua
+++ config/.config/nvim/lua/eden/modules/theme.lua
@@ -31,8 +31,6 @@ M.plugins = {
         overlay_opacity = 75,
         opacity_step = 1,
         keys = {
-          brightness_up = "<C-Up>",
-          brightness_down = "<C-Down>",
           toggle = "<Leader>ts",
         },
       })
