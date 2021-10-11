cac1ab16baef20a29ab1628c0422123ef601a5df
EdenEast <edenofest@gmail.com>
2021-10-11 01:28:49 -0400

    fix(nvim): nvim-cmp breaking change update
    Breaking change introduced in pr 224 of nvim-cmp. The change moved from
    using pmenu to its own floating window.

--- config/.config/nvim/lua/eden/modules/editor/cmp.lua
+++ config/.config/nvim/lua/eden/modules/editor/cmp.lua
@@ -44,8 +44,8 @@ local config = {
       select = true,
     }),
     ["<Tab>"] = function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-n>", true, true, true), "n")
+      if cmp.visible() then
+        cmp.select_next_item()
       elseif require("luasnip").expand_or_jumpable() then
         vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
       else
@@ -53,8 +53,8 @@ local config = {
       end
     end,
     ["<S-Tab>"] = function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-p>", true, true, true), "n")
+      if cmp.visible() then
+        cmp.select_prev_item()
       elseif require("luasnip").jumpable(-1) then
         vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
       else
