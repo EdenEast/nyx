92daa9070350102f56b2af71c4ea2f1be62f5923
James Simpson <edenofest@gmail.com>
2021-10-20 23:06:46 -0400

    fix(nvim): After ftplugin type errors

--- config/.config/nvim/after/ftplugin/vim.lua
+++ config/.config/nvim/after/ftplugin/vim.lua
@@ -9,7 +9,7 @@ vim.opt_local.iskeyword:remove("#")
 
 vim.opt_local.foldmethod = "marker"
 vim.opt_local.foldlevel = 0
-vim.opt_local.foldcolumn = 1
+vim.opt_local.foldcolumn = "1"
 
 vim.opt_local.shiftwidth = 2
 vim.opt_local.tabstop = 2
