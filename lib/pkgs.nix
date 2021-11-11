6f33bf5ba038a9f825ec68ef13189acd85505d67
EdenEast <edenofest@gmail.com>
2021-11-11 00:13:20 -0500

    refactor(flake): Create home-manager derivation
    This change refactors how derivations are constructed. This change
    clears the `flake.nix` file and implements just
    `homeManagerConfiguration`.

--- lib/pkgs.nix
+++
@@ -1,9 +0,0 @@
-{ inputs, lib, ... }:
-
-{
-  mkPkgs = system: import inputs.nixpkgs {
-    inherit system;
-    config.allowUnfree = true;
-    overlays = [ inputs.self.overlay ] ++ inputs.self.internal.overlays;
-  };
-}
