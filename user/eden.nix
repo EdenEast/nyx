362b594ed0c1de1d6335e6113ca1ae4af5c35859
James Simpson <edenofest@gmail.com>
2021-11-12 22:42:34 -0500

    feat(user): Add user to mkHome

---
+++ user/eden.nix
@@ -0,0 +1,6 @@
+{
+  name = "eden";
+  email = "edenofest@gmail.com";
+  signingKey = "5A038CEFD458DB47A6135B3F8316DECECB1A3F10";
+  hashedPassword = "$6$nF.UDyrpHmh6M$yKCw56auQ7Dm1FfvmQg6y3Y59mWsoiHJyAYhqF9e8nKjfeKwUoFocwHhogKUTq.A3hVe9S.smv7u1NLV/yPTd0";
+}
