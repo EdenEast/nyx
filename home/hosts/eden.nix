e558ddc9251a7912dc4dda0ec8e8a109b6951041
James Simpson <edenofest@gmail.com>
2021-11-12 22:49:07 -0500

    feat(git): Inherit user info from user attr set

--- home/hosts/eden.nix
+++ home/hosts/eden.nix
@@ -6,12 +6,6 @@
   nyx = {
     modules = {
       shell = {
-        git = {
-          userName = "James Simpson";
-          userEmail = "edenofest@gmail.com";
-          signing.key = "5A038CEFD458DB47A6135B3F8316DECECB1A3F10";
-        };
-
         gnupg = {
           enable = true;
           enableService = false;
