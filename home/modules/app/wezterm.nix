6db2018cc4c81ec354e05b60434d13eecb9f34d8
EdenEast <edenofest@gmail.com>
2021-11-14 18:51:55 -0500

    fix(wezterm): Optional package for wezterm
    This is for mac where I was having issues with building wezterm.

--- home/modules/app/wezterm.nix
+++ home/modules/app/wezterm.nix
@@ -6,6 +6,11 @@ in
 {
   options.nyx.modules.app.wezterm = {
     enable = mkEnableOption "wezterm configuration";
+    package = mkOption {
+      description = "Package for wezterm";
+      type = with types; nullOr package;
+      default = pkgs.wezterm;
+    };
     fontSize = mkOption {
       description = "Override font size";
       type = with types; nullOr int;
@@ -14,53 +19,55 @@ in
   };
 
   config = mkIf cfg.enable {
-    home.packages = [ pkgs.wezterm ];
+    home.packages = mkIf (cfg.package != null) [ cfg.package ];
     xdg.configFile."wezterm".source = ../../../config/.config/wezterm;
-    xdg.dataFile."wezterm/nyx.lua".text = let
-      fontText = if cfg.fontSize != null then ''
-        font_size = ${toString cfg.fontSize},
-      '' else "";
-      colorText = with config.nyx.modules.theme.colors; ''
-        colors = {
-          background = "#${bg}",
-          foreground = "#${fg}",
+    xdg.dataFile."wezterm/nyx.lua".text =
+      let
+        fontText =
+          if cfg.fontSize != null then ''
+            font_size = ${toString cfg.fontSize},
+          '' else "";
+        colorText = with config.nyx.modules.theme.colors; ''
+          colors = {
+            background = "#${bg}",
+            foreground = "#${fg}",
 
-          cursor_border = "#${comment}",
-          cursor_bg = "#${comment}",
-          cursor_fg = "#${bg}",
+            cursor_border = "#${comment}",
+            cursor_bg = "#${comment}",
+            cursor_fg = "#${bg}",
 
-          selection_bg = "#${comment}",
-          selection_fg = "#${fg}",
+            selection_bg = "#${comment}",
+            selection_fg = "#${fg}",
 
-          ansi = {
-            "#${base.black}",
-            "#${base.red}",
-            "#${base.green}",
-            "#${base.yellow}",
-            "#${base.blue}",
-            "#${base.magenta}",
-            "#${base.cyan}",
-            "#${base.white}"
+            ansi = {
+              "#${base.black}",
+              "#${base.red}",
+              "#${base.green}",
+              "#${base.yellow}",
+              "#${base.blue}",
+              "#${base.magenta}",
+              "#${base.cyan}",
+              "#${base.white}"
+            },
+            brights = {
+              "#${bright.black}",
+              "#${bright.red}",
+              "#${bright.green}",
+              "#${bright.yellow}",
+              "#${bright.blue}",
+              "#${bright.magenta}",
+              "#${bright.cyan}",
+              "#${bright.white}"
+            },
           },
-          brights = {
-            "#${bright.black}",
-            "#${bright.red}",
-            "#${bright.green}",
-            "#${bright.yellow}",
-            "#${bright.blue}",
-            "#${bright.magenta}",
-            "#${bright.cyan}",
-            "#${bright.white}"
-          },
-        },
+        '';
+      in
+      ''
+        return {
+          ${fontText}
+          ${colorText}
+        }
       '';
-    in
-    ''
-    return {
-      ${fontText}
-      ${colorText}
-    }
-    '';
   };
 }
 
