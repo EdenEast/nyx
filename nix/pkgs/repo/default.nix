ce86aaed50f928dbf51450e8e7cc19ad998fed97
James Simpson <edenofest@gmail.com>
2021-11-15 11:52:43 -0500

    fix(repo): Repo builds on darwin

--- nix/pkgs/repo/default.nix
+++ nix/pkgs/repo/default.nix
@@ -1,21 +1,21 @@
-{ lib, fetchFromGitHub, rustPlatform, openssl, pkg-config }:
+{ lib, fetchFromGitHub, rustPlatform, openssl, pkg-config, stdenv, libiconv, darwin }:
 
 rustPlatform.buildRustPackage rec {
   pname = "repo";
-  version = "0.1.2";
+  version = "0.1.3-dev";
 
   src = fetchFromGitHub {
     owner = "EdenEast";
     repo = "repo";
-    rev = "v${version}";
+    rev = "2379ed5a1e348f0274d0941245037a4c9f9c2dd2";
 
-    sha256 = "sha256-Ewtc80mtToNaUHkXeE+pUJ3cW7BTy9BGpmKmqboGzPc=";
+    sha256 = "sha256-pbgS9+PRvamFPMFj9DD/1iceAQv8wdWLJR2E2U8HcHs=";
   };
 
   nativeBuildInputs = [ pkg-config ];
-  buildInputs = [ openssl ];
+  buildInputs = [ openssl libiconv ] ++ (lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks;[ Security ]));
 
-  cargoSha256 = "sha256-3DtZyAVOBxl9jEJdSRJbVUGnGXKVKt533dQfmy5KuxE=";
+  cargoSha256 = "sha256-NnEMOikoxT6Fcx2jSiq/OUjRCInOINm+bD1xrxlf0Do=";
 
   meta = with lib; {
     description = "Repository management system";
