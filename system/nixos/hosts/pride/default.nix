321e025cabfc41b34ed80634a1e5ad326d8f1b67
James Simpson <edenofest@gmail.com>
2021-11-18 21:23:41 -0500

    feat(nixos): Add desktop profile

--- system/nixos/hosts/pride/default.nix
+++ system/nixos/hosts/pride/default.nix
@@ -1,47 +1,28 @@
-{ config, pkgs, ... }:
+{ ... }:
 
 {
   imports = [ ./hardware.nix ];
 
   networking.interfaces.wlp3s0.useDHCP = true;
 
-  nyx.modules = {
-    user.home = ./home.nix;
+  nyx = {
+    modules = {
+      user.home = ./home.nix;
 
-    caps.enable = true;
-    nvidia = {
-      enable = true;
-      intelBusId = "PCI:0:2:0";
-      nvidiaBusId = "PCI:1:0:0";
+      caps.enable = true;
+      nvidia = {
+        enable = true;
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
+      yubikey.enable = true;
     };
-    yubikey.enable = true;
-  };
 
-  services.printing.enable = true;
-  services.xserver = {
-    enable = true;
-    desktopManager.plasma5.enable = true;
-    displayManager.lightdm.enable = true;
-  };
-
-  fonts = {
-    fonts = with pkgs; [
-      (
-        nerdfonts.override {
-          fonts = [ "Hack" "Meslo" "UbuntuMono" ];
-        }
-      )
-    ];
-  };
-
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-
-  hardware = {
-    pulseaudio = {
-      enable = true;
-      support32Bit = true;
-      package = pkgs.pulseaudioFull;
+    profiles = {
+      desktop = {
+        enable = true;
+        laptop = true;
+      };
     };
   };
 
---
+++ system/nixos/profiles/desktop/default.nix
@@ -0,0 +1,74 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+let
+  cfg = config.nyx.profiles.desktop;
+
+  wmList = [ "awesome" ];
+  deList = [ "gnome" "plasma5" ];
+in
+{
+  options.nyx.profiles.desktop = {
+    laptop = mkOption {
+      description = "Enable features for a laptop (trackpad, battery, etc...)";
+      type = types.bool;
+      default = false;
+    };
+
+    flavor = mkOption {
+      description = "Desktop environment flavor";
+      type = types.enum (wmList ++ deList);
+      default = "awesome";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    services.printing.enable = true;
+
+    # Sound setting
+    sound.enable = true;
+    environment.systemPackages = with pkgs; [ pamixer ];
+
+    hardware = {
+      pulseaudio = {
+        enable = true;
+        support32Bit = true;
+        package = pkgs.pulseaudioFull;
+      };
+    };
+
+    # Desktop environment flavor
+    services.xserver = {
+      enable = true;
+      layout = "us";
+      libinput = mkIf cfg.laptop {
+        enable = true;
+        touchpad.tapping = true;
+      };
+
+      displayManager = {
+        lightdm.enable = cfg.flavor != "gnome";
+        gdm.enable = cfg.flavor == "gnome";
+        # session = [
+        #   {
+        #     name = "home-manager";
+        #     manage = "window";
+        #     start = ''
+        #       ${pkgs.runtimeShell} $HOME/.hm-xsession &
+        #       waitPID=$!
+        #     '';
+        #   }
+        # ];
+      };
+    };
+
+    services.xserver.desktopManager = mkIf (elem "${cfg.flavor}" deList) {
+      gnome.enable = mkIf (cfg.flavor == "gnome") true;
+      plasma5.enable = mkIf (cfg.flavor == "plasma5") true;
+    };
+
+    services.xserver.windowManager = mkIf (elem "${cfg.flavor}" wmList) {
+      awesome.enable = mkIf (cfg.flavor == "awesome") true;
+    };
+  };
+}
