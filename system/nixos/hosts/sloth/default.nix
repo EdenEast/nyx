db9e4d02756b5af61b93d6e9346b8dc0c35dfb5f
James Simpson <edenofest@gmail.com>
2021-11-18 21:23:41 -0500

    feat(nixos): Add desktop profile

--- system/nixos/hosts/sloth/default.nix
+++ system/nixos/hosts/sloth/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, modulesPath, ... }:
+{ ... }:
 
 {
   imports = [ ./hardware.nix ];
@@ -7,37 +7,18 @@
   networking.interfaces.enp0s25.useDHCP = true;
   networking.interfaces.wlp3s0.useDHCP = true;
 
-  nix.modules = {
-    user.home = ./home.nix;
-    caps.enable = true;
-    yubikey.enable = true;
-  };
+  nyx = {
+    modules = {
+      user.home = ./home.nix;
+      caps.enable = true;
+      yubikey.enable = true;
+    };
 
-  services.printing.enable = true;
-  services.xserver = {
-    enable = true;
-    desktopManager.plasma5.enable = true;
-    displayManager.lightdm.enable = true;
-  };
-
-  fonts = {
-    fonts = with pkgs; [
-      (
-        nerdfonts.override {
-          fonts = [ "Hack" "Meslo" "UbuntuMono" ];
-        }
-      )
-    ];
-  };
-
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-
-  hardware = {
-    pulseaudio = {
-      enable = true;
-      support32Bit = true;
-      package = pkgs.pulseaudioFull;
+    profiles = {
+      desktop = {
+        enable = true;
+        laptop = true;
+      };
     };
   };
 }
