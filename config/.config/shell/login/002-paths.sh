66b8d3792317b39a040cf7ebabfbd2f785193cd9
James Simpson <edenofest@gmail.com>
2021-11-26 11:01:38 -0500

    fix(zsh): Add `/run/current-system/sw/bin` to PATH
    There currently is an issue with zsh not adding the current-system to
    teh path. I think the solution for this is to add `programs.zsh.enable`
    outside of the home-manager configuration for darwin.
    
    https://github.com/nix-community/home-manager/issues/1226
    
    Currently there is an issue with collisions from the new nix version and
    zsh when it comes to completion. This is causing me to not be able to
    add `programs.zsh.enable` to my configuration.
    
    https://github.com/LnL7/nix-darwin/issues/373
    https://github.com/NixOS/nixpkgs/pull/146030
    
    This change has not made it into unstable yet.

--- config/.config/shell/login/002-paths.sh
+++ config/.config/shell/login/002-paths.sh
@@ -1,48 +1,47 @@
-
 function contains_path() {
-    # https://stackoverflow.com/a/1397020
-    [[ ":$PATH:" == *":$1:"* ]] && return 0 || return 1
+  # https://stackoverflow.com/a/1397020
+  [[ ":$PATH:" == *":$1:"* ]] && return 0 || return 1
 }
 
 function append_path() {
-    contains_path $1 || {
-        [[ -z $PATH ]] && PATH="$1" || PATH="$PATH:$1"
-    }
+  contains_path $1 || {
+    [[ -z $PATH ]] && PATH="$1" || PATH="$PATH:$1"
+  }
 }
 
-
 function prepend_path() {
-    contains_path $1 || {
-        [[ -z $PATH ]] && PATH="$1" || PATH="$1:$PATH"
-    }
+  contains_path $1 || {
+    [[ -z $PATH ]] && PATH="$1" || PATH="$1:$PATH"
+  }
 }
 
 function main() {
-    # save default path and clear path variable
-    local system_path="$PATH"
-    PATH=
+  # save default path and clear path variable
+  local system_path="$PATH"
+  PATH=
 
-    # if local_bin is defined then add all folders to path
-    [[ -d "$LOCAL_BIN" ]] && {
-        for dir in $LOCAL_BIN/* $LOCAL_BIN; do
-            [[ -d $dir ]] &&  prepend_path $dir
-        done
-    }
-
-    append_path "$HOME/.config/git/bin"
-    append_path "$HOME/.cargo/bin"
-
-
-    # adding system path back
-    # Ending solution from https://stackoverflow.com/a/15988793
-    while [ "$system_path" ]; do
-        iter=${system_path%%:*}
-        append_path "$iter"
-        # If one element is left then set system_path to empty else delete the first element and loop again
-        [ "$system_path" = "$iter" ] && system_path='' || system_path="${system_path#*:}"
+  # if local_bin is defined then add all folders to path
+  [[ -d $LOCAL_BIN ]] && {
+    for dir in $LOCAL_BIN/* $LOCAL_BIN; do
+      [[ -d $dir ]] && prepend_path $dir
     done
+  }
 
-    export PATH=$PATH
+  # This is just until `programs.zsh.enable` can be added to darwin.
+  append_path "/run/current-system/sw/bin"
+  append_path "$HOME/.config/git/bin"
+  append_path "$HOME/.cargo/bin"
+
+  # adding system path back
+  # Ending solution from https://stackoverflow.com/a/15988793
+  while [ "$system_path" ]; do
+    iter=${system_path%%:*}
+    append_path "$iter"
+    # If one element is left then set system_path to empty else delete the first element and loop again
+    [ "$system_path" = "$iter" ] && system_path='' || system_path="${system_path#*:}"
+  done
+
+  export PATH=$PATH
 }
 
 main
@@ -51,4 +50,3 @@ unset contains_path
 unset prepend_path
 unset append_path
 unset main
-
