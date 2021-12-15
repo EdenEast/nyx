cca1a002d6b88328de7468fff0f58ed22fc76642
EdenEast <edenofest@gmail.com>
2021-12-15 12:01:39 -0500

    refactor(nvim): Module takes list of ts grammars

--- home/modules/shell/neovim.nix
+++ home/modules/shell/neovim.nix
@@ -65,29 +65,41 @@ in
     ] ++ cfg.lspServers ++ cfg.debugAdaptors ++ cfg.formatters ++ cfg.extraPkgs;
 
     # Add Treesitter parsers
-    home.file = {
-      "${config.xdg.dataHome}/nvim/lib/libsqlite3.so".source = "${pkgs.sqlite.out}/lib/libsqlite3.so";
-      "${config.xdg.dataHome}/nvim/parser/bash.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-bash}/parser";
-      "${config.xdg.dataHome}/nvim/parser/c.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-c}/parser";
-      "${config.xdg.dataHome}/nvim/parser/cpp.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-cpp}/parser";
-      "${config.xdg.dataHome}/nvim/parser/css.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-css}/parser";
-      "${config.xdg.dataHome}/nvim/parser/fennel.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-fennel}/parser";
-      "${config.xdg.dataHome}/nvim/parser/go.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-go}/parser";
-      "${config.xdg.dataHome}/nvim/parser/haskell.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-haskell}/parser";
-      "${config.xdg.dataHome}/nvim/parser/html.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-html}/parser";
-      "${config.xdg.dataHome}/nvim/parser/javascript.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-javascript}/parser";
-      "${config.xdg.dataHome}/nvim/parser/json.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-json}/parser";
-      "${config.xdg.dataHome}/nvim/parser/lua.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-lua}/parser";
-      "${config.xdg.dataHome}/nvim/parser/markdown.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-markdown}/parser";
-      "${config.xdg.dataHome}/nvim/parser/nix.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-nix}/parser";
-      "${config.xdg.dataHome}/nvim/parser/ocaml.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-ocaml}/parser";
-      "${config.xdg.dataHome}/nvim/parser/python.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-python}/parser";
-      "${config.xdg.dataHome}/nvim/parser/regex.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-regex}/parser";
-      "${config.xdg.dataHome}/nvim/parser/rust.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-rust}/parser";
-      "${config.xdg.dataHome}/nvim/parser/svelte.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-svelte}/parser";
-      "${config.xdg.dataHome}/nvim/parser/scala.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-scala}/parser";
-      "${config.xdg.dataHome}/nvim/parser/typescript.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-typescript}/parser";
-      "${config.xdg.dataHome}/nvim/parser/yaml.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-yaml}/parser";
-    };
+    xdg.dataFile = with pkgs.tree-sitter-grammars; let
+      grammars = with pkgs.tree-sitter-grammars; [
+        tree-sitter-bash
+        tree-sitter-c
+        tree-sitter-c-sharp
+        tree-sitter-comment
+        tree-sitter-cpp
+        tree-sitter-css
+        tree-sitter-go
+        tree-sitter-javascript
+        tree-sitter-json
+        tree-sitter-lua
+        tree-sitter-make
+        # Currently this does not point to the correct markdown parser
+        # Correct one is: https://github.com/MDeiml/tree-sitter-markdown
+        # tree-sitter-markdown
+        tree-sitter-nix
+        tree-sitter-regex
+        tree-sitter-rust
+        tree-sitter-toml
+        tree-sitter-tsx
+        tree-sitter-typescript
+        tree-sitter-vim
+        tree-sitter-yaml
+      ];
+      parseName = x: removeSuffix "-grammar" (removePrefix "tree-sitter-" (getName x));
+      parsers = listToAttrs (map
+        (p: {
+          name = "nvim/parser/${parseName p}.so";
+          value = { source = "${p}/parser"; };
+        })
+        grammars);
+    in
+    parsers // {
+     "nvim/lib/libsqlite3.so".source = "${pkgs.sqlite.out}/lib/libsqlite3.so";
+   };
   };
 }
