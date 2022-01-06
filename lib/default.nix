0f0dec55e3799d5dde202d0563816e1d39c000d1
James Simpson <edenofest@gmail.com>
2022-01-06 12:06:08 -0500

    fix(nix): Darwin uses flakes pkgs

--- lib/default.nix
+++ lib/default.nix
@@ -204,20 +204,21 @@ rec {
           )
           (
             { config, ... }: {
-            system.activationScripts.applications.text = pkgs.lib.mkForce (
-              ''
-                  echo "setting up ~/Applications/Nix..."
-                  rm -rf ~/Applications/Nix
-                  mkdir -p ~/Applications/Nix
-                  chown ${userConf.name} ~/Applications/Nix
-                  find ${config.system.build.applications}/Applications -maxdepth 1 -type l | while read f; do
-                    src="$(/usr/bin/stat -f%Y $f)"
-                    appname="$(basename $src)"
-                    osascript -e "tell app \"Finder\" to make alias file at POSIX file \"/Users/${userConf.name}/Applications/Nix/\" to POSIX file \"$src\" with properties {name: \"$appname\"}";
-                done
-              ''
-            );
-          })
+              system.activationScripts.applications.text = pkgs.lib.mkForce (
+                ''
+                    echo "setting up ~/Applications/Nix..."
+                    rm -rf ~/Applications/Nix
+                    mkdir -p ~/Applications/Nix
+                    chown ${userConf.name} ~/Applications/Nix
+                    find ${config.system.build.applications}/Applications -maxdepth 1 -type l | while read f; do
+                      src="$(/usr/bin/stat -f%Y $f)"
+                      appname="$(basename $src)"
+                      osascript -e "tell app \"Finder\" to make alias file at POSIX file \"/Users/${userConf.name}/Applications/Nix/\" to POSIX file \"$src\" with properties {name: \"$appname\"}";
+                  done
+                ''
+              );
+            }
+          )
           (import ../system/common/modules)
           (import ../system/common/profiles)
           (import ../system/darwin/modules)
@@ -229,7 +230,7 @@ rec {
             self = inputs.self;
             user = userConf;
           in
-          { inherit inputs name self system user; };
+          { inherit inputs name self system user pkgs; };
       }
     );
 }
