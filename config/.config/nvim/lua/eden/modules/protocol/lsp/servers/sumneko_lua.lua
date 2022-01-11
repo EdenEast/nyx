d19208c3fe248f046f2fefbf23793bb7a94b77e6
James Simpson <edenofest@gmail.com>
2022-01-11 15:05:25 -0500

    fix(nvim): `sumneko_lua` installed with nvim-lsp-installer
    nvim-lsp-installer changed the way that sumneko_lua is installed and
    setup. It now just prepends to the `PATH` instead of setting the `cmd`

--- config/.config/nvim/lua/eden/modules/protocol/lsp/servers/sumneko_lua.lua
+++ config/.config/nvim/lua/eden/modules/protocol/lsp/servers/sumneko_lua.lua
@@ -3,6 +3,10 @@ local nlsp = require("lspconfig")
 local M = {}
 
 M.setup = function(config, _, opts)
+  local runtime_path = vim.split(package.path, ";")
+  table.insert(runtime_path, "lua/?.lua")
+  table.insert(runtime_path, "lua/?/init.lua")
+
   config.cmd = { "lua-language-server" } -- nix installed wraps all of the comamnd line args needed
   config.root_dir = function(filename, _)
     local cwd = vim.fn.getcwd()
@@ -20,7 +24,7 @@ M.setup = function(config, _, opts)
         -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
         version = "LuaJIT",
         -- Setup your lua path
-        path = vim.split(package.path, ";"),
+        path = runtime_path,
       },
       diagnostics = {
         -- Get the language server to recognize the `vim` global
@@ -28,10 +32,7 @@ M.setup = function(config, _, opts)
       },
       workspace = {
         -- Make the server aware of Neovim runtime files
-        library = {
-          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-          [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-        },
+        library = vim.api.nvim_get_runtime_file("", true),
       },
       -- Do not send telemetry data containing a randomized but unique identifier
       telemetry = {
@@ -40,8 +41,14 @@ M.setup = function(config, _, opts)
     },
   }
 
+  -- This used to be used for nvim-lsp-installer when it was setting the cmd
+  -- Now it just changes the path variable instead.
   if opts.cmd then
     config.cmd = opts.cmd
+  end
+
+  if opts.cmd_env then
+    config.cmd_env = opts.cmd_env
   end
 
   return config
