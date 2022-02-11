cb46aa1affb371afc061615e2258f867e9a78c27
James Simpson <edenofest@gmail.com>
2022-02-11 16:20:37 -0500

    chore(dotnet): Stop microsoft from sniffing me
    No telemetry for me plz Microsoft. Would you kindly FK off!
    
    (╯°□°）╯︵ ┻━┻

--- config/.config/shell/sh/000-override.sh
+++ config/.config/shell/sh/000-override.sh
@@ -5,3 +5,5 @@ export PROMPT_DIR_COLOR=$CL_CYAN
 export PROMPT_MODE=left
 export PROMPT_NO_INFO_LINE=true
 
+# Stop dotnet from collecting telemetry. FK OFF MICROSOFT!! (╯°□°）╯︵ ┻━┻
+export DOTNET_CLI_TELEMETRY_OPTOUT=1
