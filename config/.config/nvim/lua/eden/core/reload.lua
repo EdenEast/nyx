715ba9d6016dcfaa55caa25f9f975eedd6c4d56e
EdenEast <edenofest@gmail.com>
2022-03-07 09:19:54 -0500

    fix(nvim): Add impatient cache to config reload

--- config/.config/nvim/lua/eden/core/reload.lua
+++ config/.config/nvim/lua/eden/core/reload.lua
@@ -21,15 +21,20 @@ end
 
 -- Reload all user config lua modules
 M.reload_config = function()
+  -- Handle impatient.nvim automatically.
+  local luacache = (_G.__luacache or {}).cache
+
   for name, _ in pairs(package.loaded) do
     if name:match("^eden.") then
       package.loaded[name] = nil
+
+      if luacache then
+        luacache[name] = nil
+      end
     end
   end
 
   dofile(vim.env.MYVIMRC)
 end
-
-M.reload_config()
 
 return M
