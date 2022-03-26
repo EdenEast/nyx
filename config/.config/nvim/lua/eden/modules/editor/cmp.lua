d40dc1141880e8772960a5ef30ebae0a22566ca4
James Simpson <edenofest@gmail.com>
2022-03-25 23:12:36 -0400

    feat(nvim): Update cmp

--- config/.config/nvim/lua/eden/modules/editor/cmp.lua
+++ config/.config/nvim/lua/eden/modules/editor/cmp.lua
@@ -1,17 +1,11 @@
 local cmp = require("cmp")
-local luasnip = require("luasnip")
 local pairs = require("nvim-autopairs.completion.cmp")
 
-vim.opt.completeopt = "menuone,noselect"
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
 local config = {
   completion = {
     completeopt = "menu,menuone,noinsert",
-  },
-  snippet = {
-    expand = function(args)
-      require("luasnip").lsp_expand(args.body)
-    end,
   },
   documentation = {
     border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
@@ -41,47 +35,70 @@ local config = {
   mapping = {
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-o>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.close(),
-    ["<CR>"] = cmp.mapping.confirm({
+    ["<C-o>"] = cmp.mapping.complete(),
+    ["<cr>"] = cmp.mapping(
+      cmp.mapping.confirm({
+        behavior = cmp.ConfirmBehavior.Insert,
+        select = true,
+      }),
+      { "i", "c" }
+    ),
+    ["<tab>"] = cmp.config.disable,
+
+    -- Testing
+    ["<c-q>"] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     }),
-    ["<Tab>"] = function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end,
-    ["<S-Tab>"] = function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
+  },
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
     end,
   },
   sources = {
     { name = "nvim_lua" },
     { name = "nvim_lsp" },
-    { name = "treesitter" },
-    { name = "luasnip" },
     { name = "path" },
+    { name = "luasnip" },
+    { name = "treesitter" },
     {
       name = "buffer",
       keyword_length = 5,
       option = {
-        get_bufnr = function()
+        get_bufnr = function() -- all buffers
           return vim.api.nvim_list_bufs()
         end,
       },
     },
     { name = "crates" }, -- crates does check if file is a `Cargo.toml` file
+  },
+  sorting = {
+    comparators = {
+      cmp.config.compare.offset,
+      cmp.config.compare.exact,
+      cmp.config.compare.score,
+
+      -- copied from cmp-under, but I don't think I need the plugin for this.
+      -- I might add some more of my own.
+      function(entry1, entry2)
+        local _, entry1_under = entry1.completion_item.label:find("^_+")
+        local _, entry2_under = entry2.completion_item.label:find("^_+")
+        entry1_under = entry1_under or 0
+        entry2_under = entry2_under or 0
+        if entry1_under > entry2_under then
+          return false
+        elseif entry1_under < entry2_under then
+          return true
+        end
+      end,
+
+      cmp.config.compare.kind,
+      cmp.config.compare.sort_text,
+      cmp.config.compare.length,
+      cmp.config.compare.order,
+    },
   },
 }
 
