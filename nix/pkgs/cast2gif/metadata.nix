f287fbfdf1bf4c8d536a751d1d46be8bc9b2b76d
EdenEast <edenofest@gmail.com>
2022-05-24 11:16:58 -0400

    fix(pkgs): Cast2gif outputs correct metadata

--- nix/pkgs/cast2gif/metadata.nix
+++ nix/pkgs/cast2gif/metadata.nix
@@ -3,7 +3,7 @@
   version = "0.1.0-git";
   fetch = {
     owner = "katharostech";
-    repo = "katharostech";
+    repo = "cast2gif";
     rev = "c0ec2d4a46aef219745316791c8bcdfd79051f82";
     sha256 = "04lyb7kgkrpa827ams7a4l2jh31cc8cnsxlla4bjimv0ldrmhd3j";
   };
--- nix/pkgs/lib.sh
+++ nix/pkgs/lib.sh
@@ -8,11 +8,12 @@ curl_opts=(
 
 function get_latest_version_from_crates_io() {
   echo "Getting latest version from crates.io API" >&2
-  local create
+  local create result
 
   crate="$1"
 
-  curl "${curl_opts[@]}" "$crates_api/$crate" | jq -r .crate.max_stable_version
+  result=$(curl "${curl_opts[@]}" "$crates_api/$crate" | jq -r .crate.max_stable_version)
+  echo "Version = $result" >&2 && echo "$result"
 }
 
 function get_latest_version_from_github() {
@@ -22,24 +23,26 @@ function get_latest_version_from_github() {
   user="$1"
   repo="$2"
 
-  curl "${curl_opts[@]}" "$gh_api/$user/$repo/releases/latest" | jq -r .tag_name
+  result=$(curl "${curl_opts[@]}" "$gh_api/$user/$repo/releases/latest" | jq -r .tag_name)
+  echo "Version = $result" >&2 && echo "$result"
 }
 
 function get_head_from_github() {
   echo "Getting HEAD from github API" >&2
-  local user repo branch
+  local user repo branch result
 
   user="$1"
   repo="$2"
 
-  set +u;
-  branch="$3";
-  if [[ -z "$3" ]]; then
+  set +u
+  branch="$3"
+  if [[ -z $3 ]]; then
     branch=$(curl "${curl_opts[@]}" "$gh_api/$user/$repo" | jq -r .default_branch)
   fi
   set -u
 
-  curl "${curl_opts[@]}" "$gh_api/$user/$repo/branches/$branch" | jq -r .commit.sha
+  result=$(curl "${curl_opts[@]}" "$gh_api/$user/$repo/branches/$branch" | jq -r .commit.sha)
+  echo "rev = $result" >&2 && echo "$result"
 }
 
 function prefetch_from_crates_io() {
