569d81be75d04089f12ad1170f6dfa6d8ecac63b
James Simpson <edenofest@gmail.com>
2022-06-14 00:29:50 -0400

    fix(nvim): Update nvim-tree deprications

--- config/.config/nvim/lua/eden/modules/nav/nvimtree.lua
+++ config/.config/nvim/lua/eden/modules/nav/nvimtree.lua
@@ -1,30 +1,34 @@
 local tree_cb = require("nvim-tree.config").nvim_tree_callback
 
-vim.g.nvim_tree_git_hl = 1
 vim.g.nvim_tree_width_allow_resize = 0
-vim.g.nvim_tree_add_trailing = 1
-vim.g.nvim_tree_group_empty = 1
 -- vim.g.nvim_tree_bindings = {
 --   { key = "l", cb = tree_cb("edit") },
 --   { key = "h", cb = tree_cb("close_node") },
 -- }
-vim.g.nvim_tree_icons = {
-  default = "",
-  symlink = "",
-  git = {
-    unstaged = "✚",
-    staged = "✚",
-    unmerged = "≠",
-    renamed = "≫",
-    untracked = "★",
-  },
-}
 
 require("nvim-tree").setup({
   ignore_ft_on_setup = { ".git", "node_modules" },
   tab_open = true,
   update_focused_file = {
     enable = true,
+  },
+  render = {
+    add_trailing = true,
+    group_empty = true,
+    highlight_git = true,
+    icons = {
+      glyphs = {
+        default = "",
+        symlink = "",
+        git = {
+          unstaged = "✚",
+          staged = "✚",
+          unmerged = "≠",
+          renamed = "≫",
+          untracked = "★",
+        },
+      },
+    },
   },
   view = {
     mappings = {
