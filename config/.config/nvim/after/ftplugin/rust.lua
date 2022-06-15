1ec2449e124bfc115de52433c922055f47640c80
EdenEast <edenofest@gmail.com>
2022-06-15 18:52:31 -0400

    feat(nvim): Set compiler to cargo for rust
    This change also adds keymaps to run `make`
    command with cargo and results outputing to
    `quickfix` list.

--- config/.config/nvim/after/ftplugin/rust.lua
+++ config/.config/nvim/after/ftplugin/rust.lua
@@ -5,3 +5,25 @@ vim.opt_local.expandtab = true
 
 --  Vim ftplugin sets `o` ... why? I dont want!
 vim.opt_local.formatoptions:remove("o")
+
+-- make compiler cargo
+vim.cmd("compiler cargo")
+
+-- Make quickfix window show up automatically after compiling
+vim.api.nvim_create_autocmd("QuickFixCmdPost", {
+  pattern = "[^l]*",
+  command = "cwindow",
+  nested = true,
+})
+
+vim.api.nvim_create_autocmd("QuickFixCmdPost", {
+  pattern = "l*",
+  command = "lwindow",
+  nested = true,
+})
+
+edn.keymap({
+  { "<leader>mt", "<cmd>make test -q<cr>" },
+  { "<leader>mb", "<cmd>make build<cr>" },
+  { "<leader>mc", "<cmd>make clippy -q<cr>" },
+})
