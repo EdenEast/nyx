d8a6d7d6b568d3648503880796bad35b26c5be8f
James Simpson <edenofest@gmail.com>
2022-06-17 16:07:03 -0400

    fix(nvim): Update keymap lib

--- config/.config/nvim/lua/eden/lib/keymap.lua
+++ config/.config/nvim/lua/eden/lib/keymap.lua
@@ -1,31 +1,113 @@
+local default_opts = { noremap = true }
 local M = {}
 
-function M.map(mode, lhs, rhs, opts)
-  vim.keymap.set(mode, lhs, rhs, opts)
+local function err(msg)
+  vim.notify(msg, vim.log.levels.ERROR)
 end
 
-function M.nmap(lhs, rhs, opts)
-  vim.keymap.set("n", lhs, rhs, opts)
+local function validate(input, exec)
+  vim.validate({
+    input = { input, { "string", "table" } },
+    exec = { exec, { "string", "function" } },
+  })
 end
 
-function M.imap(lhs, rhs, opts)
-  vim.keymap.set("i", lhs, rhs, opts)
+local function merge_opts(opts)
+  if opts and type(opts) == "table" then
+    opts = vim.tbl_extend("force", default_opts, opts)
+  else
+    opts = default_opts
+  end
+
+  return opts
 end
 
-function M.vmap(lhs, rhs, opts)
-  vim.keymap.set("v", lhs, rhs, opts)
+local function mapper(mode, input, exec, opts)
+  local ok, errmsg = pcall(validate, input, exec)
+  if not ok then
+    err(errmsg)
+    return
+  end
+
+  opts = merge_opts(opts)
+
+  ok, errmsg = pcall(vim.keymap.set, mode, input, exec, opts)
+  if not ok then
+    err(errmsg)
+  end
 end
 
-function M.xmap(lhs, rhs, opts)
-  vim.keymap.set("x", lhs, rhs, opts)
+---Create a keymap with either a string or table of modes.
+---@param mode string|table
+---@param input string
+---@param exec string|function
+---@param opts table
+function M.map(mode, input, exec, opts)
+  mapper(mode, input, exec, opts)
 end
 
-function M.cmap(lhs, rhs, opts)
-  vim.keymap.set("c", lhs, rhs, opts)
+---Create a normal mode keymap. See :help mapmode-n
+---@param input string
+---@param exec string|function
+---@param opts table
+function M.nmap(input, exec, opts)
+  mapper("n", input, exec, opts)
 end
 
-function M.tmap(lhs, rhs, opts)
-  vim.keymap.set("t", lhs, rhs, opts)
+---Create a insert mode keymap. See :help mapmode-n
+---@param input string
+---@param exec string|function
+---@param opts table
+function M.imap(input, exec, opts)
+  mapper("i", input, exec, opts)
+end
+
+---Create a visual mode keymap. See :help mapmode-v
+---@param input string
+---@param exec string|function
+---@param opts table
+function M.vmap(input, exec, opts)
+  mapper("v", input, exec, opts)
+end
+
+---Create a command mode keymap. See :help mapmode-c
+---@param input string
+---@param exec string|function
+---@param opts table
+function M.cmap(input, exec, opts)
+  mapper("c", input, exec, opts)
+end
+
+---Create a visual and select mode keymap. See :help mapmode-x
+---@param input string
+---@param exec string|function
+---@param opts table
+function M.xmap(input, exec, opts)
+  mapper("x", input, exec, opts)
+end
+
+---Create a operator-pending mode keymap. See :help mapmode-o
+---@param input string
+---@param exec string|function
+---@param opts table
+function M.omap(input, exec, opts)
+  mapper("o", input, exec, opts)
+end
+
+---Create a select mode keymap. See :help mapmode-s
+---@param input string
+---@param exec string|function
+---@param opts table
+function M.smap(input, exec, opts)
+  mapper("s", input, exec, opts)
+end
+
+---Create a terminal mode keymap. See :help mapmode-t
+---@param input string
+---@param exec string|function
+---@param opts table
+function M.tmap(input, exec, opts)
+  mapper("t", input, exec, opts)
 end
 
 return M
