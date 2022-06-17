52f66c92dff8c7594b6f67cb58f44b55fa52b9a5
EdenEast <edenofest@gmail.com>
2022-06-17 00:16:58 -0400

    chore(nvim): Change to proper lua

--- config/.config/nvim/lua/eden/bootstrap.lua
+++ config/.config/nvim/lua/eden/bootstrap.lua
@@ -46,21 +46,21 @@ local pack = require("eden.core.pack")
 -- Disable some of the distributed plugins that are
 -- shipped with neovim
 local function disable_distibution_plugins()
-  vim.g.loaded_gzip = 1
-  vim.g.loaded_tar = 1
-  vim.g.loaded_tarPlugin = 1
-  vim.g.loaded_zip = 1
-  vim.g.loaded_zipPlugin = 1
-  vim.g.loaded_getscript = 1
-  vim.g.loaded_getscriptPlugin = 1
-  vim.g.loaded_vimball = 1
-  vim.g.loaded_vimballPlugin = 1
-  vim.g.loaded_2html_plugin = 1
-  vim.g.loaded_rrhelper = 1
-  vim.g.loaded_netrw = 1
-  vim.g.loaded_netrwPlugin = 1
-  vim.g.loaded_netrwSettings = 1
-  vim.g.loaded_netrwFileHandlers = 1
+  vim.g.loaded_gzip = true
+  vim.g.loaded_tar = true
+  vim.g.loaded_tarPlugin = true
+  vim.g.loaded_zip = true
+  vim.g.loaded_zipPlugin = true
+  vim.g.loaded_getscript = true
+  vim.g.loaded_getscriptPlugin = true
+  vim.g.loaded_vimball = true
+  vim.g.loaded_vimballPlugin = true
+  vim.g.loaded_2html_plugin = true
+  vim.g.loaded_rrhelper = true
+  vim.g.loaded_netrw = true
+  vim.g.loaded_netrwPlugin = true
+  vim.g.loaded_netrwSettings = true
+  vim.g.loaded_netrwFileHandlers = true
 end
 
 -- Initalize runtimepath to contain the following locations
