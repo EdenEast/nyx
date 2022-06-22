e6ba51892f65996f18bb24dd2e2ab7636e63e011
James Simpson <edenofest@gmail.com>
2022-06-22 11:35:54 -0400

    feat(nvim): Update format on save to use `lsp.buf.format`

--- config/.config/nvim/lua/eden/modules/protocol/lsp/filetypes.lua
+++ config/.config/nvim/lua/eden/modules/protocol/lsp/filetypes.lua
@@ -4,6 +4,15 @@ local M = {
       [[autocmd BufEnter,BufWritePost <buffer> :lua require("eden.modules.protocol.lsp.extensions.inlay_hints").request({aligned = true, prefix = " » "}) ]]
     )
   end,
+
+  lua = function()
+    local filter = function(clients)
+      return vim.tbl_filter(function(client)
+        return client.name ~= "sumneko_lua"
+      end, clients)
+    end
+    require("eden.modules.protocol.lsp.extensions.format").add_ft_filter("lua", filter, false)
+  end,
 }
 
 return setmetatable(M, {
