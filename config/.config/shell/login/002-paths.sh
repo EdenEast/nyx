690c9df7018d442a9d893df0f64630d281b38728
EdenEast <edenofest@gmail.com>
2022-06-26 12:40:55 -0400

    fix(sh): Prepend local cargo bin folder to path
    This is useful when I have built local cargo applications and I want to
    use them instead of a global installed one. This is useful for when I am
    working with projects like nushell and want nightly (I cant get a nix
    overlay working).

--- config/.config/shell/login/002-paths.sh
+++ config/.config/shell/login/002-paths.sh
@@ -29,6 +29,7 @@ function main() {
 
   append_path "$HOME/.config/git/bin"
   append_path "$HOME/.cargo/bin"
+  append_path "$HOME/.local/share/cargo/bin"
 
   # adding system path back
   # Ending solution from https://stackoverflow.com/a/15988793
