3636f2a5dc7fb57b3b5fa8fb81952cf3c510e76e
James Simpson <edenofest@gmail.com>
2022-07-05 10:38:07 -0400

    chore(nvim): Remove treesitter autotag

--- config/.config/nvim/lua/eden/modules/protocol/treesitter.lua
+++ config/.config/nvim/lua/eden/modules/protocol/treesitter.lua
@@ -4,7 +4,6 @@ vim.api.nvim_command("set foldexpr=nvim_treesitter#foldexpr()")
 local deps = {
   "nvim-ts-context-commentstring",
   "nvim-treesitter-textobjects",
-  "nvim-ts-autotag",
 }
 
 if not edn.platform.is_windows then
@@ -12,16 +11,11 @@ if not edn.platform.is_windows then
 end
 require("eden.lib.defer").immediate_load(deps)
 
-vim.cmd([[packadd nvim-ts-context-commentstring]])
-vim.cmd([[packadd nvim-treesitter-textobjects]])
-vim.cmd([[packadd nvim-ts-autotag]])
-
 -- Setting clang as the compiler to use as pre this solution
 -- https://github.com/nvim-treesitter/nvim-treesitter/wiki/Windows-support#troubleshooting
 -- NOTE: Had issues with clang and nix installed build-essentials and gcc and it works
 require("nvim-treesitter.install").compilers = { "gcc", "clang", "cl" }
 require("nvim-treesitter.configs").setup({
-  autotag = { enable = true },
   highlight = { enable = true, disable = { "yaml" } },
   playground = { enable = true },
   query_linter = {
