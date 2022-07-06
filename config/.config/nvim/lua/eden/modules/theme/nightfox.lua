2fdafbe321cc1dca19abe7ea878958239283e367
James Simpson <edenofest@gmail.com>
2022-07-05 23:59:32 -0400

    chore(nvim): Disable unused nightfox modules

--- config/.config/nvim/lua/eden/modules/theme/nightfox.lua
+++ config/.config/nvim/lua/eden/modules/theme/nightfox.lua
@@ -1,4 +1,20 @@
 require("nightfox").setup({
+  options = {
+    modules = {
+      aerial = false,
+      barbar = false,
+      dashboard = false,
+      fern = false,
+      glyph_palette = false,
+      hop = false,
+      lsp_saga = false,
+      modes = false,
+      neotree = false,
+      pounce = false,
+      sneak = false,
+      tsrainbow = false,
+    },
+  },
   specs = {
     all = {
       syntax = {
