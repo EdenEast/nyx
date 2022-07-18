9a50dae4742800e71e154555ae600e32a1fc99d8
EdenEast <edenofest@gmail.com>
2022-07-18 09:58:12 -0400

    docs(mac): Document default mouse acceleration

--- home/modules/theme/default.nix
+++ home/modules/theme/default.nix
@@ -22,3 +22,10 @@ in
     nyx.modules.theme.colors = with builtins; fromJSON (readFile ((toString ./.) + "/${cfg.name}.json"));
   };
 }
+
+# Default mouse acceleration
+# defaults read .GlobalPreferences com.apple.mouse.scaling
+# Result: 0.875
+#
+# Disable mouse acceleration
+# defaults write .GlobalPreferences com.apple.mouse.scaling 0
