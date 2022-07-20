4b435ad4385b1216492189c572d74e2c09c6638e
James Simpson <edenofest@gmail.com>
2022-07-20 16:43:48 -0400

    feat(nvim): Add short rev to neovim version
    Because neovim is taken from a tarball it does not have the commit hash
    in the version. This is hard to determine which commit neovim was built
    from.
    
    This change patches neovim to add the short rev to the version name for
    when there is no git folder and just a tarball.

--- nix/overlays/neovim/default.nix
+++ nix/overlays/neovim/default.nix
@@ -2,8 +2,20 @@
 _final: prev:
 
 {
-  neovim-unwrapped = inputs.neovim-flake.packages.${prev.system}.neovim;
-  neovim-nightly = inputs.neovim-flake.packages.${prev.system}.neovim;
-  neovim-debug = inputs.neovim-flake.packages.${prev.system}.neovim-debug;
-  neovim-developer = inputs.neovim-flake.packages.${prev.system}.neovim-developer;
+  neovim-unwrapped = inputs.neovim-flake.packages.${prev.system}.neovim.overrideAttrs (old: {
+    patches = old.patches ++ [ ./0001-Add-nix-short-rev-to-pre-release-version-info.patch ];
+    NIX_SHORT_REV = inputs.neovim-flake.shortRev;
+  });
+  neovim-nightly = inputs.neovim-flake.packages.${prev.system}.neovim.overrideAttrs (old: {
+    patches = old.patches ++ [ ./0001-Add-nix-short-rev-to-pre-release-version-info.patch ];
+    NIX_SHORT_REV = inputs.neovim-flake.shortRev;
+  });
+  neovim-debug = inputs.neovim-flake.packages.${prev.system}.neovim-debug.overrideAttrs (old: {
+    patches = old.patches ++ [ ./0001-Add-nix-short-rev-to-pre-release-version-info.patch ];
+    NIX_SHORT_REV = inputs.neovim-flake.shortRev;
+  });
+  neovim-developer = inputs.neovim-flake.packages.${prev.system}.neovim-developer.overrideAttrs (old: {
+    patches = old.patches ++ [ ./0001-Add-nix-short-rev-to-pre-release-version-info.patch ];
+    NIX_SHORT_REV = inputs.neovim-flake.shortRev;
+  });
 }
