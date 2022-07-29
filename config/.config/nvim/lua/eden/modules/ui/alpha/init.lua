e59c64c0a6eff935fe96d5a163a5ef467c3f4f77
EdenEast <edenofest@gmail.com>
2022-07-28 23:38:34 -0400

    fix(nvim): Update alpha for windows

--- config/.config/nvim/lua/eden/modules/ui/alpha/init.lua
+++ config/.config/nvim/lua/eden/modules/ui/alpha/init.lua
@@ -2,9 +2,14 @@ local alpha = require("alpha")
 local dashboard = require("alpha.themes.dashboard")
 local logos = require("eden.modules.ui.alpha.logo")
 
+local function get_user()
+  local env = require('eden.core.platform').is_windows and "USERNAME" or "USER"
+  return os.getenv(env)
+end
+
 local function footer()
   local datetime = os.date(" %Y-%m-%d") .. "  -  "
-  local author = " " .. os.getenv("USER") .. "  -  "
+  local author = " " .. get_user() .. "  -  "
   local total_plugins = " " .. #vim.tbl_keys(packer_plugins) .. " plugins" .. "  -  "
   local version = vim.version()
   local nvim_version_info = " v" .. version.major .. "." .. version.minor .. "." .. version.patch
