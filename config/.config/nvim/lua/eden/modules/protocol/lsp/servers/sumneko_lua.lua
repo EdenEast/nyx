b22c231e4488d957988677c3d43d0ab44dd81705
EdenEast <edenofest@gmail.com>
2022-08-05 12:11:02 -0400

    fix(nvim): Stop sumneko_lua from asking about love

--- config/.config/nvim/lua/eden/modules/protocol/lsp/servers/sumneko_lua.lua
+++ config/.config/nvim/lua/eden/modules/protocol/lsp/servers/sumneko_lua.lua
@@ -33,6 +33,7 @@ M.setup = function(config, on_attach, opts)
       workspace = {
         -- Make the server aware of Neovim runtime files
         library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
       },
       -- Do not send telemetry data containing a randomized but unique identifier
       telemetry = {
