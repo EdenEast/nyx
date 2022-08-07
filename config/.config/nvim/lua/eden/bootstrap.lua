7635d71dd5920875217b4e97194e00bb1eb67d18
EdenEast <edenofest@gmail.com>
2022-08-07 12:49:45 -0400

    fix(nvim): Re-enable match paren

--- config/.config/nvim/lua/eden/bootstrap.lua
+++ config/.config/nvim/lua/eden/bootstrap.lua
@@ -53,7 +53,7 @@ local function disable_distibution_plugins()
   vim.g.loaded_vimball = 1
   vim.g.loaded_vimballPlugin = 1
   -- vim.g.loaded_matchit = 1
-  vim.g.loaded_matchparen = 1
+  -- vim.g.loaded_matchparen = 1
   vim.g.loaded_2html_plugin = 1
   vim.g.loaded_logiPat = 1
   vim.g.loaded_rrhelper = 1
