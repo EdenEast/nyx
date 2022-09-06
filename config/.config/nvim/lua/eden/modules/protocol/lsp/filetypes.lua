f17456522a5226a3e28b0aca01b9932f62b31213
EdenEast <edenofest@gmail.com>
2022-09-06 11:18:50 -0400

    fix(nvim): Update format filter for lua

--- config/.config/nvim/lua/eden/modules/protocol/lsp/filetypes.lua
+++ config/.config/nvim/lua/eden/modules/protocol/lsp/filetypes.lua
@@ -6,8 +6,10 @@ local M = {
   end,
 
   lua = function(_)
-    local filter = function(client)
-      return client.name ~= "sumneko_lua"
+    local filter = function(clients)
+      return vim.tbl_filter(function(client)
+        return client.name ~= "sumneko_lua"
+      end, clients)
     end
     require("eden.modules.protocol.lsp.extensions.format").add_ft_filter("lua", filter, false)
   end,
