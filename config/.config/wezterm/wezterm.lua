cf30765d6a1d9efa88b42d010a5f383693c5f74e
EdenEast <edenofest@gmail.com>
2022-12-08 14:26:06 -0500

    fix(wezterm): remove default fallback colorscheme
    It seems that wezterm changed the priority of colors and color_schemes.
    Removing the default fallback colors table.

--- config/.config/wezterm/wezterm.lua
+++ config/.config/wezterm/wezterm.lua
@@ -70,20 +70,7 @@ local config = {
     join(datahome, "colors"),
   },
 
-  -- Default colorscheme is nightfox
-  colors = {
-    background = "#192330",
-    foreground = "#cdcecf",
-
-    cursor_border = "#cdcecf",
-    cursor_bg = "#cdcecf",
-    cursor_fg = "#192330",
-
-    selection_bg = "#223249",
-    selection_fg = "#cdcecf",
-    ansi = { "#393b44", "#c94f6d", "#81b29a", "#dbc074", "#719cd6", "#9d79d6", "#63cdcf", "#dfdfe0" },
-    brights = { "#575860", "#d16983", "#8ebaa4", "#e0c989", "#86abdc", "#baa1e2", "#7ad4d6", "#e4e4e5" },
-  },
+  color_scheme = "nightfox",
 
   exit_behavior = "Close",
 
