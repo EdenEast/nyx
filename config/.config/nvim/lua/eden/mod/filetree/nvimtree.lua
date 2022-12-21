8cdf02339acc4d4448170dbffdb0997fb2ccf777
EdenEast <edenofest@gmail.com>
2022-12-21 14:29:42 -0500

    feat(nvim): add feline to lazy

--- config/.config/nvim/lua/eden/mod/filetree/nvimtree.lua
+++ config/.config/nvim/lua/eden/mod/filetree/nvimtree.lua
@@ -1,6 +1,6 @@
 return {
   "kyazdani42/nvim-tree.lua",
-  dependencies = { "kyazdani42/nvim-web-devicons" },
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     local tree_cb = require("nvim-tree.config").nvim_tree_callback
     vim.g.nvim_tree_width_allow_resize = 0
