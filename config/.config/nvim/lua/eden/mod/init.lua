cc78c50aae8bc6a85ab2e855e8c17097167bf778
EdenEast <edenofest@gmail.com>
2022-12-20 22:31:50 -0500

    feat(nvim): add misc plugins

--- config/.config/nvim/lua/eden/mod/init.lua
+++ config/.config/nvim/lua/eden/mod/init.lua
@@ -1,1 +1,14 @@
-return {}
+return {
+  -- These are some plugins that are misc
+  {
+    -- "tweekmonster/startuptime.vim",
+    "dstein64/vim-startuptime",
+    cmd = { "StartupTime" },
+  },
+  { "editorconfig/editorconfig-vim" },
+  { "christoomey/vim-tmux-navigator" },
+  {
+    "kevinhwang91/nvim-bqf",
+    ft = "qf",
+  },
+}
