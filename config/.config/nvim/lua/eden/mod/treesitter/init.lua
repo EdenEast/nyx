569b25990da5f020e0b89802f3dfa76cf029ae58
EdenEast <edenofest@gmail.com>
2022-12-20 21:04:00 -0500

    feat(nvim): migrade treesitter to lazy

---
+++ config/.config/nvim/lua/eden/mod/treesitter/init.lua
@@ -0,0 +1,74 @@
+local M = {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  event = "BufReadPost",
+  dependencies = {
+    "romgrk/nvim-treesitter-context",
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
+}
+
+M.config = function()
+  -- Setting clang as the compiler to use as pre this solution
+  -- https://github.com/nvim-treesitter/nvim-treesitter/wiki/Windows-support#troubleshooting
+  -- NOTE: Had issues with clang and nix installed build-essentials and gcc and it works
+  require("nvim-treesitter.install").compilers = { "gcc", "clang", "cl" }
+  require("nvim-treesitter.configs").setup({
+    highlight = { enable = true },
+    playground = { enable = true },
+    query_linter = {
+      enable = true,
+      use_virtual_text = true,
+      lint_events = { "BufWrite", "CursorHold" },
+    },
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          ["ia"] = "@parameter.inner",
+          ["aa"] = "@parameter.outer",
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+        },
+      },
+    },
+    context_commentstring = {
+      enable = true,
+      config = {
+        c = "// %s",
+        lua = "-- %s",
+        nix = "# %s",
+      },
+    },
+    ensure_installed = {
+      "bash",
+      "c",
+      "cmake",
+      "c_sharp",
+      "comment",
+      "cpp",
+      "go",
+      "javascript",
+      "json",
+      "lua",
+      "nix",
+      "python",
+      "regex",
+      "rust",
+      "toml",
+      "typescript",
+      "tsx",
+    },
+  })
+
+  vim.opt.foldmethod = "expr"
+  vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+  if not edn.platform.is.win then
+    require("treesitter-context").setup()
+  end
+end
+
+return M
