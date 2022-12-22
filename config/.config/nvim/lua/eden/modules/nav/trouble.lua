ecc631e0eff8b76b797425fb6211ae6aa69e7c32
EdenEast <edenofest@gmail.com>
2022-12-22 15:33:54 -0500

    fix(nvim): remove remaining old packer module folder

--- config/.config/nvim/lua/eden/modules/nav/trouble.lua
+++
@@ -1,9 +0,0 @@
-local trouble = require("trouble")
-
-trouble.setup()
-
-nmap("<leader>xx", [[<cmd>TroubleToggle<cr>]], { desc = "Toggle trouble" })
-nmap("<leader>xw", [[<cmd>TroubleToggle lsp_workspace_diagnostics<cr>]], { desc = "Workspace diagnostics" })
-nmap("<leader>xd", [[<cmd>TroubleToggle lsp_document_diagnostics<cr>]], { desc = "Document diagnostics" })
-nmap("<leader>xl", [[<cmd>TroubleToggle locallist<cr>]], { desc = "Local list" })
-nmap("<leader>xq", [[<cmd>TroubleToggle quickfix<cr>]], { desc = "Quickfix list" })
