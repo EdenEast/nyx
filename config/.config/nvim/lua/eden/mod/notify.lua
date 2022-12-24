969ad0e136a6ef07a13e7e6fae351d337a2b6dc5
EdenEast <edenofest@gmail.com>
2022-12-23 23:33:46 -0500

    feat(nvim): add whichkey and notify plugin

---
+++ config/.config/nvim/lua/eden/mod/notify.lua
@@ -0,0 +1,36 @@
+return {
+  "rcarriga/nvim-notify",
+  config = function()
+    local nvim_notify = require("notify")
+    nvim_notify.setup({
+      -- Animation style (see below for details)
+      stages = "slide",
+
+      -- Function called when a new window is opened, use for changing win settings/config
+      on_open = nil,
+
+      -- Function called when a window is closed
+      on_close = nil,
+
+      -- Render function for notifications. See notify-render()
+      render = "default",
+
+      -- Default timeout for notifications
+      timeout = 2000,
+
+      -- Max number of columns for messages
+      max_width = nil,
+      -- Max number of lines for a message
+      max_height = nil,
+
+      -- For stages that change opacity this is treated as the highlight behind the window
+      -- Set this to either a highlight group, an RGB hex value e.g. "#000000" or a function returning an RGB code for dynamic values
+      background_colour = "Normal",
+
+      -- Minimum width for notification windows
+      minimum_width = 36,
+    })
+
+    vim.notify = require("notify")
+  end,
+}
