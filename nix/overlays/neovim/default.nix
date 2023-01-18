7617ad217f0a25c4909d7ba9b3f2a583c8f3e9b0
EdenEast <edenofest@gmail.com>
2023-01-18 01:06:31 -0500

    build(nix): remove incompatible reproducible patch
    NixOS/nixpkgs#208103 introduced a patch that make builds reproducible.
    This patch applies cleanly against 0.8.2, but doesn't apply against
    master.
    
    Since my config targets nightly neovim I am filtering out this patch.

--- nix/overlays/neovim/default.nix
+++ nix/overlays/neovim/default.nix
@@ -1,21 +1,32 @@
 { inputs, ... }:
 _final: prev:
 
+# NixOS/nixpkgs#208103 introduced a patch that makes builds reproduceable.
+# This patch applies to the current 0.8.2 but does not apply to nightly.
+# Filtering out this patch.
+let
+  patchFilter = v: builtins.filter
+    (p:
+      let patch = if builtins.typeOf p == "set" then baseNameOf p.name else baseNameOf p;
+      in patch != "neovim-build-make-generated-source-files-reproducible.patch"
+    )
+    v;
+in
 {
   neovim-unwrapped = inputs.neovim-flake.packages.${prev.system}.neovim.overrideAttrs (old: {
-    patches = old.patches ++ [ ./0001-Add-nix-short-rev-to-pre-release-version-info.patch ];
+    patches = patchFilter old.patches ++ [ ./0001-Add-nix-short-rev-to-pre-release-version-info.patch ];
     NIX_SHORT_REV = inputs.neovim-flake.shortRev;
   });
   neovim-nightly = inputs.neovim-flake.packages.${prev.system}.neovim.overrideAttrs (old: {
-    patches = old.patches ++ [ ./0001-Add-nix-short-rev-to-pre-release-version-info.patch ];
+    patches = patchFilter old.patches ++ [ ./0001-Add-nix-short-rev-to-pre-release-version-info.patch ];
     NIX_SHORT_REV = inputs.neovim-flake.shortRev;
   });
   neovim-debug = inputs.neovim-flake.packages.${prev.system}.neovim-debug.overrideAttrs (old: {
-    patches = old.patches ++ [ ./0001-Add-nix-short-rev-to-pre-release-version-info.patch ];
+    patches = patchFilter old.patches ++ [ ./0001-Add-nix-short-rev-to-pre-release-version-info.patch ];
     NIX_SHORT_REV = inputs.neovim-flake.shortRev;
   });
   neovim-developer = inputs.neovim-flake.packages.${prev.system}.neovim-developer.overrideAttrs (old: {
-    patches = old.patches ++ [ ./0001-Add-nix-short-rev-to-pre-release-version-info.patch ];
+    patches = patchFilter old.patches ++ [ ./0001-Add-nix-short-rev-to-pre-release-version-info.patch ];
     NIX_SHORT_REV = inputs.neovim-flake.shortRev;
   });
 }
