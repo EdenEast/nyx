2506aa92d518200df36c9cbeb0d0ab33adcbd86f
EdenEast <edenofest@gmail.com>
2023-03-27 00:24:26 -0400

    feat(nvim): add ftplugin options for filetypes

---
+++ config/.config/nvim/after/ftplugin/cs.lua
@@ -0,0 +1,2 @@
+-- Vim ftplugin sets `o` ... why? I dont want!
+vim.opt_local.formatoptions:remove("o")
---
+++ config/.config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,17 @@
+vim.opt_local.conceallevel = 0
+vim.opt_local.expandtab = true
+vim.opt_local.shiftwidth = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.spell = true
+vim.opt_local.tabstop = 2
+vim.opt_local.textwidth = 120
+
+vim.opt_local.spell = true
+vim.opt_local.spelllang = { "en_us" }
+
+require("cmp").setup.buffer({
+  sources = {
+    { name = "emoji" },
+    { name = "spell" },
+  },
+})
