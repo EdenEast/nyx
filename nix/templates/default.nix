6d1132bb44e3566fa4c2a5628c643d55d865bd10
EdenEast <edenofest@gmail.com>
2023-04-13 15:50:37 -0400

    feat(nix): add shell and simple templates

--- nix/templates/default.nix
+++ nix/templates/default.nix
@@ -3,4 +3,12 @@
     path = ./rust;
     description = "Rust applicationa and env";
   };
+  simple = {
+    path = ./simple;
+    description = "Simple flake setup";
+  };
+  shell = {
+    path = ./shell;
+    description = "Simple nix-shell";
+  };
 }
