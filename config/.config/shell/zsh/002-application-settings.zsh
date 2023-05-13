8c8ac43e12b966c51a8ca7619f0b15718f2aece0
Xavier Young <45989017+younger-1@users.noreply.github.com>
2023-05-13 12:37:08 +0800

    Update 002-application-settings.zsh

--- config/.config/shell/zsh/002-application-settings.zsh
+++ config/.config/shell/zsh/002-application-settings.zsh
@@ -1,9 +1,9 @@
-# if repo is installed then soure the work script
+# if repo is installed then source the work script
 [[ -x "$(command -v repo)" ]] && {
     [ -x "$(command -v fzf)" ] && eval "$(repo init zsh --fzf)" || eval "$(repo init zsh)"
 }
 
-# if zoxide is insatlled then source helper scripts
+# if zoxide is installed then source the helper script
 [[ -x "$(command -v zoxide)" ]] && {
     eval "$(zoxide init zsh)"
 }
