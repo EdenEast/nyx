96a962b8ad9d7a95c343193c2335ff4e9ddfa158
James Simpson <edenofest@gmail.com>
2023-06-12 23:54:39 -0400

    fix(windows): promote script to admin for compat wsl disk

--- windows/bin/compact-wsl-disk.ps1
+++ windows/bin/compact-wsl-disk.ps1
@@ -1,5 +1,37 @@
 # https://github.com/mikemaccana/compact-wsl2-disk/blob/2dd8cadae7d5f8bcd6f0c085bb981964427c6828/compact-wsl2-disk.ps1
 
+# Get the ID and security principal of the current user account
+$myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+$myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
+
+# Get the security principal for the Administrator role
+$adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+
+# Check to see if we are currently running "as Administrator"
+if ($myWindowsPrincipal.IsInRole($adminRole)) {
+    # We are running "as Administrator" - so change the title and background color to indicate this
+    $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
+    $Host.UI.RawUI.BackgroundColor = "DarkBlue"
+    clear-host
+} else {
+    # We are not running "as Administrator" - so relaunch as administrator
+
+    # Create a new process object that starts PowerShell
+    $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+
+    # Specify the current script path and name as a parameter
+    $newProcess.Arguments = $myInvocation.MyCommand.Definition;
+
+    # Indicate that the process should be elevated
+    $newProcess.Verb = "runas";
+
+    # Start the new process
+    [System.Diagnostics.Process]::Start($newProcess);
+
+    # Exit from the current, unelevated, process
+    # exit
+}
+
 $ErrorActionPreference = "Stop"
 
 # The array containing the files to compress
