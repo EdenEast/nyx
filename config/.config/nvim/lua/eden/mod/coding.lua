a4acf6a9f2317955333db48b04b99aab6677bfc6
EdenEast <edenofest@gmail.com>
2023-07-05 15:54:22 -0400

    feat(nvim): change from leap to flash for ezmotion

--- config/.config/nvim/lua/eden/mod/coding.lua
+++ config/.config/nvim/lua/eden/mod/coding.lua
@@ -163,22 +163,55 @@ return {
   },
 
   -- better jumping / easymotion
+  -- {
+  --   "ggandor/leap.nvim",
+  --   keys = {
+  --     { "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
+  --     { "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
+  --     { "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
+  --   },
+  --   config = function(_, opts)
+  --     local leap = require("leap")
+  --     for k, v in pairs(opts) do
+  --       leap.opts[k] = v
+  --     end
+  --     leap.add_default_mappings(true)
+  --     vim.keymap.del({ "x", "o" }, "x")
+  --     vim.keymap.del({ "x", "o" }, "X")
+  --   end,
+  -- },
+
   {
-    "ggandor/leap.nvim",
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
     keys = {
-      { "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
-      { "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
-      { "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump({
+            search = {
+              mode = function(str) return "\\<" .. str end,
+            },
+          })
+        end,
+        desc = "Flash",
+      },
+      -- Not sure about this one
+      -- {
+      --   "S",
+      --   mode = { "n", "o", "x" },
+      --   function() require("flash").treesitter() end,
+      --   desc = "Flash Treesitter",
+      -- },
+      {
+        "r",
+        mode = "o",
+        function() require("flash").remote() end,
+        desc = "Remote Flash",
+      },
     },
-    config = function(_, opts)
-      local leap = require("leap")
-      for k, v in pairs(opts) do
-        leap.opts[k] = v
-      end
-      leap.add_default_mappings(true)
-      vim.keymap.del({ "x", "o" }, "x")
-      vim.keymap.del({ "x", "o" }, "X")
-    end,
   },
 
   -- better text-objects
