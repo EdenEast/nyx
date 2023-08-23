73bb225eff5c6a236ccd26e553cf104e79529e49
EdenEast <edenofest@gmail.com>
2023-08-23 11:42:56 -0400

    fix(nvim): remove char and search modes for flash

--- config/.config/nvim/lua/eden/mod/coding.lua
+++ config/.config/nvim/lua/eden/mod/coding.lua
@@ -149,18 +149,18 @@ return {
   },
 
   -- easily jump to any location and enhanced f/t motions for Leap
-  {
-    "ggandor/flit.nvim",
-    keys = function()
-      ---@type LazyKeys[]
-      local ret = {}
-      for _, key in ipairs({ "f", "F", "t", "T" }) do
-        ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
-      end
-      return ret
-    end,
-    opts = { labeled_modes = "nx" },
-  },
+  -- {
+  --   "ggandor/flit.nvim",
+  --   keys = function()
+  --     ---@type LazyKeys[]
+  --     local ret = {}
+  --     for _, key in ipairs({ "f", "F", "t", "T" }) do
+  --       ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
+  --     end
+  --     return ret
+  --   end,
+  --   opts = { labeled_modes = "nx" },
+  -- },
 
   -- better jumping / easymotion
   -- {
@@ -184,7 +184,12 @@ return {
   {
     "folke/flash.nvim",
     event = "VeryLazy",
-    opts = {},
+    opts = {
+      modes = {
+        char = { enabled = false },
+        search = { enabled = false },
+      },
+    },
     keys = {
       {
         "s",
