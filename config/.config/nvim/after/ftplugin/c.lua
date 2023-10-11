3017c15cfc3842c063842604d10916c67d8b19d3
EdenEast <edenofest@gmail.com>
2023-10-11 16:35:40 -0400

    fix(nvim): use // for comments in c

--- config/.config/nvim/after/ftplugin/c.lua
+++ config/.config/nvim/after/ftplugin/c.lua
@@ -5,3 +5,6 @@ vim.opt_local.expandtab = true
 
 -- Vim ftplugin sets `o` ... why? I dont want!
 vim.opt_local.formatoptions:remove("o")
+
+-- Use single line comments for comment motions
+vim.opt_local.commentstring = "// %s"
