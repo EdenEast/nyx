1699ed70393139426166503face813c4ae417e1c
EdenEast <edenofest@gmail.com>
2024-01-24 10:35:35 -0500

    fix(nvim): clean up treesitter config

--- config/.config/nvim/lua/eden/mod/treesitter.lua
+++ config/.config/nvim/lua/eden/mod/treesitter.lua
@@ -5,17 +5,20 @@ return {
     event = { "BufReadPost", "BufNewFile" },
     dependencies = {
       "nvim-treesitter/nvim-treesitter-textobjects",
-      "JoosepAlviste/nvim-ts-context-commentstring",
     },
     config = function()
       -- Setting clang as the compiler to use as pre this solution
       -- https://github.com/nvim-treesitter/nvim-treesitter/wiki/Windows-support#troubleshooting
       -- NOTE: Had issues with clang and nix installed build-essentials and gcc and it works
-      require("nvim-treesitter.install").compilers = { "gcc", "clang", "cl" }
+      -- require("nvim-treesitter.install").compilers = { "gcc", "clang", "cl" }
       require("nvim-treesitter.configs").setup({
+        ensure_installed = { "c", "lua", "rust", "vim", "vimdoc", "query" },
+
+        sync_install = false,
+        auto_install = true,
+
         highlight = { enable = true },
         indent = { enable = true },
-        playground = { enable = true },
         textobjects = {
           select = {
             enable = true,
@@ -27,42 +30,6 @@ return {
               ["if"] = "@function.inner",
             },
           },
-        },
-        context_commentstring = {
-          enable = true,
-          enable_autocmd = false,
-          config = {
-            c = "// %s",
-            lua = "-- %s",
-            nix = "# %s",
-          },
-        },
-        ensure_installed = {
-          "bash",
-          "c",
-          "cpp",
-          "c_sharp",
-          "comment",
-          "dhall",
-          "html",
-          "javascript",
-          "json",
-          "lua",
-          "luadoc",
-          "luap",
-          "markdown",
-          "markdown_inline",
-          "nix",
-          "python",
-          "query",
-          "regex",
-          "rust",
-          "toml",
-          "tsx",
-          "typescript",
-          "vim",
-          "vimdoc",
-          "yaml",
         },
       })
 
@@ -79,16 +46,5 @@ return {
       vim.opt.foldmethod = "expr"
       vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
     end,
-  },
-
-  {
-    "nvim-treesitter/playground",
-    keys = {
-      {
-        "<leader>uH",
-        function() require("nvim-treesitter-playground.hl-info").show_hl_captures() end,
-        desc = "Highlight groups",
-      },
-    },
   },
 }
