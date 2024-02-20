8ab899449eadd4e08a63a931d1193985b3ab350c
EdenEast <edenofest@gmail.com>
2024-02-20 00:33:25 -0500

    feat(nvim): add obsidian for neovim

--- config/.config/nvim/lua/eden/mod/editor.lua
+++ config/.config/nvim/lua/eden/mod/editor.lua
@@ -94,6 +94,14 @@ return {
     },
   },
 
+  {
+    "epwalsh/obsidian.nvim",
+    lazy = true,
+    ft = "markdown",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function() require("obsidian").setup({}) end,
+  },
+
   -- references
   {
     "RRethy/vim-illuminate",
