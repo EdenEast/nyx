94a33a3e5ef626bda21a6ec3a5bfb96497534999
EdenEast <edenofest@gmail.com>
2024-06-01 10:18:20 -0400

    feat(wrath): add fingerprint support

--- system/nixos/hosts/wrath/default.nix
+++ system/nixos/hosts/wrath/default.nix
@@ -1,4 +1,4 @@
-{ inputs, pkgs, ... }:
+{ inputs, pkgs, config, ... }:
 
 {
   imports = [
@@ -12,6 +12,29 @@
   # A workaround has been upstreamed in Linux v6.7. For older kernels, the following works around this, with the
   # trade-off that keyboard presses also no longer wake the system.
   hardware.framework.amd-7040.preventWakeOnAC = true;
+
+  # The firmware on the fingerprint sensor needs a downgrade to make it work on Linux. The process is documented here.
+  # However on recent NixOS versions also fwupd can no longer update the firmware. Using the following snippet allows
+  # to temporarly downgrade fwupd to an old-enough version:
+  services.fwupd = {
+    enable = true;
+
+    # we need fwupd 1.9.7 to downgrade the fingerprint sensor firmware
+    package = (import
+      (builtins.fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/bb2009ca185d97813e75736c2b8d1d8bb81bde05.tar.gz";
+        sha256 = "sha256:003qcrsq5g5lggfrpq31gcvj82lb065xvr7bpfa8ddsw8x4dnysk";
+      })
+      {
+        inherit (pkgs) system;
+      }).fwupd;
+  };
+
+  environment.systemPackages = with pkgs; [
+    libnotify
+    kitty
+    lutris
+  ];
 
   nyx = {
     modules = {
@@ -35,7 +58,4 @@
 
   programs.steam.enable = true;
   hardware.opengl.driSupport32Bit = true;
-  environment.systemPackages = with pkgs; [
-    lutris
-  ];
 }
