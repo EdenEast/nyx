336d923ed846842decb6f308b2e83453259883ad
EdenEast <edenofest@gmail.com>
2025-07-14 09:45:50 -0400

    feat: update tmux navigation and profiler

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -1,1 +1,22 @@
+if vim.env.NVIM_PROFILE then
+  local pack = vim.fn.stdpath("cache") .. "/lazy/pack/profile.nvim"
+  vim.opt.rtp:prepend(pack)
+  vim.api.nvim_create_autocmd("UiEnter", {
+    group = vim.api.nvim_create_augroup("startup_profiler_trace", { clear = true }),
+    callback = function()
+      local profile = require("profile")
+      profile.stop()
+      vim.ui.input({ prompt = "Save profile to:", completion = "file", default = "profile.json" }, function(filename)
+        if filename then
+          profile.export(filename)
+          vim.notify(string.format("Wrote %s", filename))
+        end
+      end)
+    end,
+  })
+  local profile = require("profile")
+  profile.instrument_autocmds()
+  profile.start("*")
+end
+
 require("eden.core")
