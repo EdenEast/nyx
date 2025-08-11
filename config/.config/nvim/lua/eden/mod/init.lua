ea5e4c794f8fba4863515cddea8a210b1f8f4617
EdenEast <edenofest@gmail.com>
2025-08-11 15:40:33 -0400

    fix(nvim): use nvim-tmux-navigation

--- config/.config/nvim/lua/eden/mod/init.lua
+++ config/.config/nvim/lua/eden/mod/init.lua
@@ -10,5 +10,19 @@ return {
   },
 
   -- tmux navigator
-  { "christoomey/vim-tmux-navigator" },
+{
+    "alexghergh/nvim-tmux-navigation",
+    config = function()
+      local ntn = require("nvim-tmux-navigation")
+      ntn.setup({
+        disable_when_zoomed = true, -- defaults to false
+      })
+      local k = vim.keymap.set
+      k("n", "<C-h>", ntn.NvimTmuxNavigateLeft)
+      k("n", "<C-j>", ntn.NvimTmuxNavigateDown)
+      k("n", "<C-k>", ntn.NvimTmuxNavigateUp)
+      k("n", "<C-l>", ntn.NvimTmuxNavigateRight)
+    end,
+  },
+
 }
