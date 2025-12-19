2af5ab8ab59164fc704e312ed2d76a175f079143
EdenEast <edenofest@gmail.com>
2025-12-18 19:12:23 -0500

    chore: remove old configuration

--- home/modules/app/default.nix
+++
@@ -1,3 +0,0 @@
-{...}: {
-  imports = [./ghostty ./alacritty.nix ./discord.nix ./firefox.nix ./kitty.nix ./obs.nix ./obsidian.nix ./wezterm.nix ./zen-browser.nix];
-}
--- nix/pkgs/cargo-workspace/metadata.nix
+++
@@ -1,9 +0,0 @@
-{
-  pname = "cargo-workspace";
-  version = "0.1.0";
-  fetch = {
-    pname = "cargo-workspace";
-    version = "0.1.0";
-    sha256 = "0jqwyagl3zl1gd373h671wqwanwnbn1m0dv3ilaywlgrsjr832f2";
-  };
-}
