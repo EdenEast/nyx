a7c0172aba2688b9e8c2e9c9edaf5bc3c7a2dcf9
EdenEast <edenofest@gmail.com>
2025-12-29 15:45:30 -0500

    home/services/dms: add Dank Material Shell to niri

--- flake.nix
+++ flake.nix
@@ -65,6 +65,12 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # Quickshell
+    dankMaterialShell = {
+      url = "github:AvengeMedia/DankMaterialShell/stable";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
