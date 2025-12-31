5686b383a590acb11426163e53e71c47ce0b5ddb
EdenEast <edenofest@gmail.com>
2025-12-31 16:54:23 -0500

    home/programs/fish: add escape hatch for experimentation

--- modules/home/programs/fish/functions.nix
+++ modules/home/programs/fish/functions.nix
@@ -1,18 +1,22 @@
 _: {
-  mkcd = ''
-    if test (count $argv) -ne 1
-      echo "Usage: mkcd <directory>"
-      return 1
-    end
-    mkdir -p $argv[1] && cd $argv[1]
-  '';
+  mkcd =
+    # fish
+    ''
+      if test (count $argv) -ne 1
+        echo "Usage: mkcd <directory>"
+        return 1
+      end
+      mkdir -p $argv[1] && cd $argv[1]
+    '';
 
   # Process killer by name
-  killall-fuzzy = ''
-    if test (count $argv) -eq 0
-      echo "Usage: killall-fuzzy <process_name_pattern>"
-      return 1
-    end
-    ps aux | grep $argv[1] | grep -v grep | awk '{print $2}' | xargs -r kill
-  '';
+  killall-fuzzy =
+    # fish
+    ''
+      if test (count $argv) -eq 0
+        echo "Usage: killall-fuzzy <process_name_pattern>"
+        return 1
+      end
+      ps aux | grep $argv[1] | grep -v grep | awk '{print $2}' | xargs -r kill
+    '';
 }
