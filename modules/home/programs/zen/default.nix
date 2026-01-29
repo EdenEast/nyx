8b19af2fa6310ff808b7f786ceb1adb3c3c84e78
EdenEast <edenofest@gmail.com>
2026-01-29 12:59:42 -0500

    docs: zen browser new window breaking functionality
    In version v1.18b ([changelog])  zen browser introduced `window sync`
    which synced tabs across all windows BREAKING the reason you would want
    to create create a new window. The change was made with no setting in
    the settings menu to disable it (wild that a fundamental change like
    this was made with no easy user setting to change it).
    
    The change was introduced in [#10034][PR] and the latest [comment] from
    the maintainer is to set `zen.window-sync.enabled` to false.
    
    For reference here are some other areas of discussion:
    - https://github.com/zen-browser/desktop/issues/7079
      - initial issue that started the window-sync work
    - https://github.com/zen-browser/desktop/discussions/12025 (locked)
      - locked discussion after zen updated users to new "feature"
    
    [changelog]: https://zen-browser.app/release-notes/#1.18b
    [PR]: https://github.com/zen-browser/desktop/pull/10034
    [comment]: https://github.com/zen-browser/desktop/pull/10034#issuecomment-3809427483

--- modules/home/programs/zen/default.nix
+++ modules/home/programs/zen/default.nix
@@ -12,5 +12,32 @@
 
   config = lib.mkIf config.my.home.programs.zen.enable {
     programs.zen-browser.enable = true;
+
+    # Notes:
+    # require to change the following in `about:config`
+    # - `zen.window-sync.enabled` to `false`
+    #
+    #   Introduced in version v1.18b (https://zen-browser.app/release-notes/#1.18b)
+    #   Zen now sync the ENTIRE window across multiple windows. This change breaks
+    #   the REASON to have multiple windows as tabs are now also synced across windows
+    #   making creating a new window entirly pointless.
+    #
+    #   At the time of investigation it looks like the maintainer stating that this is the
+    #   expected defaults and has not put a setting to disable it The only solution that
+    #   stops the undesired behaviour is outlined here.
+    #   https://github.com/zen-browser/desktop/pull/10034#issuecomment-3809427483
+    #
+    #   This however breaks other things like theming, esential and pinned tabs.
+    #   The version before this change should be the expected behavour and this toggle does
+    #   not get back to that state.
+    #
+    #   The issue is that a browser should not be pinned at a later verison if we do not
+    #   like the change as that is a security consern.
+    #
+    #
+    # - `zen.window-sync.prefer-unsynced-windows` to `true`
+    #
+    #   There is also this config value but unsure which is the correct resolution.
+    #   https://github.com/zen-browser/desktop/issues/12008#issuecomment-3795584534
   };
 }
