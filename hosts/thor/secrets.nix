f66c5c2f3d36d5cdccf8f88d1880dfad6412c98b
EdenEast <edenofest@gmail.com>
2026-03-16 14:21:28 -0400

    secrets: add root_thor publicKeys

--- hosts/thor/secrets.nix
+++ hosts/thor/secrets.nix
@@ -1,6 +1,5 @@
-{self, ...}: {
+_: {
   age.secrets = {
-    cloudflared-matrix.file = "${self.secretsDir}/cloudflared-tunnel-matrix.age";
     tailscaleAuthKey.file = ../../secrets/tailscale-auth.age;
   };
 }
