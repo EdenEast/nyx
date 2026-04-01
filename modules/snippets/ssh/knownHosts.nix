a0f8bd55d9978ec9350ecfe061fe76b8147e214f
EdenEast <edenofest@gmail.com>
2026-04-01 15:51:02 -0400

    chore: add thor to known hosts

--- modules/snippets/ssh/knownHosts.nix
+++ modules/snippets/ssh/knownHosts.nix
@@ -13,6 +13,10 @@
         hostNames = ["wrath" "wrath.local" "wrath.${config.my.snippets.tailnet.name}"];
         publicKeyFile = "${self.secretsDir}/publicKeys/root_wrath.pub";
       };
+      thor = {
+        hostNames = ["thor" "thor.local" "thor.${config.my.snippets.tailnet.name}"];
+        publicKeyFile = "${self.secretsDir}/publicKeys/root_thor.pub";
+      };
     };
   };
 }
