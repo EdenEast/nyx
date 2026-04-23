92ccd2f7eb1f11e59b7657f3863b18ea6c3d77c9
EdenEast <edenofest@gmail.com>
2026-04-23 00:45:36 -0400

    devshell: add per-host nixos-rebuild wrapper scripts

--- modules/flake/shell.nix
+++ modules/flake/shell.nix
@@ -1,20 +1,43 @@
-_: {
+{self, ...}: {
   perSystem = {
     config,
     pkgs,
     inputs',
     ...
-  }: {
+  }: let
+    nixosHosts = builtins.attrNames self.nixosConfigurations;
+    mkHostScript = host:
+      pkgs.writeShellScriptBin host ''
+        target="${host}"
+        subcmd="''${1:build}"
+        shift || true
+
+        if [ "$(hostname)" = "$target" ]; then
+          sudo nixos-rebuild --flake . "$subcmd" "$@"
+        else
+          case "$subcmd" in
+            build)
+              colmena build --on "$target" "$subcmd" "$@"
+              ;;
+            *)
+              colmena apply --on "$target" "$subcmd" "$@"
+              ;;
+          esac
+        fi
+      '';
+  in {
     devShells.default = pkgs.mkShell {
       inputsFrom = [config.flake-root.devShell];
       name = "nyx";
-      packages = with pkgs; [
-        git-crypt
-        nh
-        watchexec
-        inputs'.colmena.packages.colmena
-        inputs'.ragenix.packages.default
-      ];
+      packages =
+        (with pkgs; [
+          git-crypt
+          nh
+          watchexec
+          inputs'.colmena.packages.colmena
+          inputs'.ragenix.packages.default
+        ])
+        ++ map mkHostScript nixosHosts;
     };
   };
 }
